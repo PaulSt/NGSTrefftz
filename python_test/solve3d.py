c2b3666c53e7b614fa7178e14fbe759d4dbb499c
Paul <stocker.paul@hotmail.com>
2018-03-15 15:36:20 +0100

    first try of timeslab solving

--- python_test/solve3d.py
+++ python_test/solve3d.py
@@ -1,9 +1,9 @@
 #########################################################################################################################################
-N = 4
-c=2
-t_steps = 1/5
-order = 5
-k = 1
+c = 2
+t_steps = 1/10
+nt_steps = 5
+order = 3
+k = 2
 #########################################################################################################################################
 from netgen.geom2d import unit_square
 from netgen.csg import unit_cube
@@ -16,32 +16,53 @@ from DGeq import *
 # mesh = Mesh(unit_cube.GenerateMesh(maxh = 0.2,quad_dominated=True))
 ngmeshbase = unit_square.GenerateMesh(maxh = 0.4)
 mesh = ProdMesh(ngmeshbase,t_steps)
-Draw(mesh)
+#Draw(mesh)
 #########################################################################################################################################
 truesol =  sin( k*(c*z+x+y) )#exp(-pow(c*x+y,2)))#
 v0 = c*k*cos(k*(c*z+x+y))#grad(U0)[0]
 sig0 = CoefficientFunction( (-k*cos(k*(c*z+x+y)),-k*cos(k*(c*z+x+y))) )#-grad(U0)[1]
-
 # for order in range(3,order):
 fes = FESpace("trefftzfespace", mesh, order = order, wavespeed = c, dgjumps=True, basistype=0)
 # fes = L2(mesh, order=order, flags = { "dgjumps" : True })
-U0 = GridFunction(fes)
-U0.Set(truesol)
+# U0 = GridFunction(fes)
+# U0.Set(truesol)basemesh = Mesh(ngmeshbase)
 # Draw(U0)
 # input()
 
-[a,f] = DGeqsys(fes,truesol,v0,sig0,c)
 
-gfu = DGsolve(fes,a,f)
 
-L2error = Integrate((truesol - gfu)*(truesol - gfu), mesh)
-sH1error = Integrate((grad(U0) - grad(gfu))*(grad(U0) - grad(gfu)), mesh)
-print("error=", L2error)
-print("grad-error=", sH1error)
+
+
+
+basemesh = Mesh(ngmeshbase)
+Draw(basemesh)
+gfu = GridFunction(fes)
+gfu.Set(truesol)
+input("startcomp")
+for t_step in range(nt_steps):
+	truesol = sin( k*(c* (z+t_step*t_steps) +x+y) )
+	gD = c*k* cos( k*(c* (z+t_step*t_steps) +x+y) )
+	v0 = c*k*cos(k*(c*(z+t_step*t_steps)+x+y))
+	sig0 = CoefficientFunction( (-k*cos(k*(c*(z+t_step*t_steps)+x+y)),-k*cos(k*(c*(z+t_step*t_steps)+x+y))) )
+	[a,f] = DGeqsys(fes,truesol,v0,sig0,c,gD)
+	[gfu, cond] = DGsolve(fes,a,f)
+
+	#sig0 = CoefficientFunction((-grad(gfu)[0],-grad(gfu)[1]))
+	#v0 = grad(gfu)[2]
+
+	L2error = Integrate((truesol - gfu)*(truesol - gfu), mesh)
+	#gradtruesol = CoefficientFunction(( k*cos(k*(c*z+x+y)), k*cos(k*(c*z+x+y)), c*k*cos(k*(c*z+x+y)) ))
+	#sH1error = sqrt(Integrate((gradtruesol - grad(gfu))*(gradtruesol - grad(gfu)), mesh))
+	print("error=", L2error)
+	#print("grad-error=", sH1error)
+
+	Draw(gfu,basemesh,'sol')
+	input(t_step)
+
 # Draw(gfu,mesh,'sol')
 # Draw(grad(gfu),mesh,'gradsol')
 
-#basemesh = Mesh(ngmeshbase)
+#
 #Draw(gfu,basemesh,'sol')
 
 #for time in np.arange(0.0, t_steps, 0.1):
