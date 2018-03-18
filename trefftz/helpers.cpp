93acb8980be5358fd41d186bdf433b2489878677
PaulSt <stocker.paul@hotmail.com>
2018-03-18 22:18:08 +0100

    starting on Horner method

--- trefftz/helpers.cpp
+++ trefftz/helpers.cpp
@@ -1,51 +1,17 @@
-#ifndef FILE_HELPERS
-#define FILE_HELPERS
-#include <cmath>
+#include "helpers.hpp"
 
 namespace ngfem
 {
-  constexpr long ipow (int base, int expo, int result = 1)
+
+  double Horner (Vector<double> a, int x)
   {
-    return expo < 1 ? result
-                    : ipow (base * base, expo / 2,
-                            (expo % 2) ? result * base : result);
+    int deg = a.Size () - 1;
+    double result = a[deg];
+    for (int i = deg - 1; i >= 0; --i)
+      result = result * x + a[i];
+    return result;
   }
 
-  inline constexpr long BinCoeff (int n, int k)
-  {
-    return round (tgamma (n + 1) / (tgamma (k + 1) * tgamma (n - k + 1)));
-  }
+  double MultiHorner (Vector<double> a, Vector<double> x, int deg) {}
 
-  // k-th coeff of Legendre polynomial of degree n in monomial basis
-  constexpr double LegCoeffMonBasis (int n, int k)
-  {
-    if (n == 0)
-      return 1;
-    if (k > n)
-      return 0;
-    if ((n + k) % 2)
-      return 0;
-    double coeff = pow (2, -n) * pow (-1, floor ((n - k) / 2))
-                   * BinCoeff (n, floor ((n - k) / 2)) * BinCoeff (n + k, n);
-    // double coeff = pow(2,-n) * pow(-1,k) * BinCoeff(n,k) *
-    // BinCoeff(2*n-2*k,n);
-    return coeff;
-  }
-
-  // k-th coeff of Chebyshev polynomial of degree n in monomial basis
-  constexpr double ChebCoeffMonBasis (int n, int k)
-  {
-    if (n == 0)
-      return 1;
-    if (k > n)
-      return 0;
-    if ((n + k) % 2)
-      return 0;
-    double coeff = pow (2, k - 1) * n * pow (-1, floor ((n - k) / 2))
-                   * tgamma ((n + k) / 2)
-                   / (tgamma (floor ((n - k) / 2) + 1) * tgamma (k + 1));
-    return coeff;
-  }
 }
-
-#endif
