ff0c41e7b21e4d98ab1bcd638803e95e25846242
PaulSt <stocker.paul@hotmail.com>
2018-04-06 14:59:34 +0200

    repaired other basistypes

--- trefftz/TrefftzElement.cpp
+++ trefftz/TrefftzElement.cpp
@@ -138,29 +138,37 @@ namespace ngfem
                       }
                     basisstorage (l, i) *= 1.0 / (k * (k - 1));
                   }
-                else if (k <= 1 && l == 0
-                         && setbasis
-                                <= i) // if((k == 0 && l < BinCoeff(D-1 + ord,
-                                      // ord)) || (k == 1 && l >= BinCoeff(D-1
-                                      // + ord, ord))) //time=0 and =1
+                else if (k <= 1) // time=0 and =1
                   {
-                    double coeff = 1;
                     switch (basistype)
                       {
                       case 0:
-                        basisstorage (setbasis++, i)
-                            = 1.0; // set the l-th coeff to 1
+                        if (l == 0 && setbasis <= i)
+                          basisstorage (setbasis++, i)
+                              = 1.0; // set the l-th coeff to 1
                         // i += nbasis-1;	//jump to time = 2 if i=0
                         break;
                       case 1:
-                        for (int exponent : indices.Row (i).Range (0, D - 1))
-                          coeff *= LegCoeffMonBasis (l, exponent);
-                        basisstorage (l, i) = coeff;
+                        if ((k == 0 && l < BinCoeff (D - 1 + ord, ord))
+                            || (k == 1 && l >= BinCoeff (D - 1 + ord, ord)))
+                          {
+                            basisstorage (l, i) = 1;
+                            for (int exponent :
+                                 indices.Row (i).Range (0, D - 1))
+                              basisstorage (l, i)
+                                  *= LegCoeffMonBasis (l, exponent);
+                          }
                         break;
                       case 2:
-                        for (int exponent : indices.Row (i).Range (0, D - 1))
-                          coeff *= ChebCoeffMonBasis (l, exponent);
-                        basisstorage (l, i) = coeff;
+                        if ((k == 0 && l < BinCoeff (D - 1 + ord, ord))
+                            || (k == 1 && l >= BinCoeff (D - 1 + ord, ord)))
+                          {
+                            basisstorage (l, i) = 1;
+                            for (int exponent :
+                                 indices.Row (i).Range (0, D - 1))
+                              basisstorage (l, i)
+                                  *= ChebCoeffMonBasis (l, exponent);
+                          }
                         break;
                       }
                   }
@@ -169,7 +177,6 @@ namespace ngfem
         order = ord;
         btype = basistype;
       }
-
     return basisstorage;
   }
 
