6ecdcb7fa47e1e5acef118dbea42a117f3273748
Paul <stocker.paul@hotmail.com>
2018-06-13 12:13:55 +0200

    function call in c++ from py

--- trefftz/TestPython.hpp
+++ trefftz/TestPython.hpp
@@ -7,19 +7,13 @@
 int TestPython ()
 {
   PyObject *pName, *pModule, *pDict, *pFunc;
-  string PlugInPath = "~/trefftzngs/trefftz/EvolveTent.py";
-  string plugInInitCode;
-  // Py_SetProgramName(argv[0]);  [> optional but recommended <]
-  Py_Initialize ();
-  PyRun_SimpleString ("from time import time,ctime\n"
-                      "print('Today is',ctime(time()))\n");
-  plugInInitCode = "import sys\nimport os\nsys.path.insert(0,\"";
-  plugInInitCode += PlugInPath;
-  plugInInitCode += "\")";
-  PyRun_SimpleString ("import sys\nsys.path.append(\".\")");
-  const char *plugInCode = "EvolveTent";
-  pName = PyUnicode_FromString (plugInCode);
-  pModule = PyImport_Import (pName);
+  py::module test = py::module::import ("EvolveTent");
+  py::object bla = test.attr ("add") (1, 2);
+  // Py_Initialize();
+  // PyRun_SimpleString("import sys\nsys.path.append(\".\")");
+  // const char* plugInCode = "EvolveTent";
+  // pName = PyUnicode_FromString(plugInCode);
+  // pModule = PyImport_Import(pName);
   return 4;
 }
 
