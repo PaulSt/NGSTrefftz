abd86fabcbb93ad653bbb0a6ecf7385dab0d8355
Paul <stocker.paul@hotmail.com>
2018-06-19 23:09:29 +0200

    building inner TP mesh

--- python_test/tentpitching.py
+++ python_test/tentpitching.py
@@ -2,7 +2,7 @@ from netgen.geom2d import unit_square
 from netgen.csg import unit_cube
 from prodmesh import *
 from ngsolve import *
-#  import netgen.gui
+import netgen.gui
 from trefftzngs import *
 from DGeq import *
 import time
@@ -13,6 +13,7 @@ from ngsolve.comp import *
 #  mesh = PeriodicProdMesh(ngmeshbase,t_stepsize)
 
 mesh = Mesh(SegMesh(4,0,0.5))
+tpmesh = ngs_tpmesh(mesh,1)
+Draw(tpmesh)
 # mesh2 = Mesh(unit_square.GenerateMesh(maxh=0.4))
 # mesh = Mesh(MakeTensorProductMesh(mesh2,mesh1))
-EvolveTents(mesh)
