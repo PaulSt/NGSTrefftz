4dd62b6adc14529a817909a8af3c61753a668b20
Paul <ping.paul@hotmail.com>
2018-10-09 11:33:25 +0200

    switching to mip in D+1 dim space

--- trefftz/evolvetent.cpp
+++ trefftz/evolvetent.cpp
@@ -421,15 +421,14 @@ namespace ngcomp
     for (int elnr = 0; elnr < ma->GetNE (); elnr++)
       {
         HeapReset hr (lh);
-        MappedIntegrationRule<D, D> mir (ir, ma->GetTrafo (elnr, lh),
-                                         lh); // <dim  el, dim space>
+        MappedIntegrationRule<D, D + 1> mir (ir, ma->GetTrafo (elnr, lh),
+                                             lh); // <dim  el, dim space>
         for (int imip = 0; imip < mir.Size (); imip++)
           {
-            Vec<D + 1> p;
-            p.Range (0, D) = mir[imip].GetPoint ();
-            p[D] = time;
+            mir[imip].Point () (D) = time;
             int offset = elnr * ir.Size () * (D + 2) + imip * (D + 2);
-            ic.Range (offset, offset + D + 2) = TestSolution<D> (p, wavespeed);
+            ic.Range (offset, offset + D + 2)
+                = TestSolution<D> (mir[imip].Point (), wavespeed);
           }
       }
     return ic;
@@ -447,14 +446,12 @@ namespace ngcomp
     for (int elnr = 0; elnr < ma->GetNE (); elnr++)
       {
         HeapReset hr (lh);
-        MappedIntegrationRule<D, D> mir (ir, ma->GetTrafo (elnr, lh),
-                                         lh); // <dim  el, dim space>
         for (int imip = 0; imip < ir.Size (); imip++)
           {
             int offset = elnr * ir.Size () * (D + 2) + imip * (D + 2);
             l2error += (wavefront (offset) - wavefront_corr (offset))
                        * (wavefront (offset) - wavefront_corr (offset))
-                       * mir[imip].GetWeight ();
+                       * ir[imip].Weight ();
           }
       }
     return sqrt (l2error);
