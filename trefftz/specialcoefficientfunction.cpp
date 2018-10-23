374409091dc7db7c40f3554758e088f27d37e2c2
Paul <ping.paul@hotmail.com>
2018-10-23 14:08:34 +0200

    added matrix option for integrationpointfunction

--- trefftz/specialcoefficientfunction.cpp
+++ trefftz/specialcoefficientfunction.cpp
@@ -56,6 +56,13 @@ void ExportSpecialCoefficientFunction (py::module m)
             new (instance) IntegrationPointFunction (mesh, intrule, data);
           },
           py::arg ("mesh"), py::arg ("intrule"), py::arg ("Vector"))
+      .def (
+          "__init__",
+          [] (IntegrationPointFunction *instance, shared_ptr<MeshAccess> mesh,
+              IntegrationRule &intrule, Matrix<> data) {
+            new (instance) IntegrationPointFunction (mesh, intrule, data);
+          },
+          py::arg ("mesh"), py::arg ("intrule"), py::arg ("Matrix"))
 
       .def ("PrintTable", &IntegrationPointFunction::PrintTable);
 
