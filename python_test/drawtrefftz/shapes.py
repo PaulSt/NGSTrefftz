ca466d646756089d63174acc908cc89bd173c2cb
Paul <ping.paul@hotmail.com>
2018-11-02 14:09:20 +0100

    cleanup of drawing shapes

--- python_test/drawtrefftz/shapes.py
+++ python_test/drawtrefftz/shapes.py
@@ -1,41 +1,22 @@
 # Test the new exported coefficientfunction
-
+import netgen.gui
 from netgen.geom2d import unit_square
+from netgen.csg import unit_cube
 from ngsolve import *
 from trefftzngs import *
-
-from netgen.csg import unit_cube
 
 # mesh = Mesh(unit_square.GenerateMesh(maxh=0.4))
 mesh = Mesh(unit_cube.GenerateMesh(maxh = 0.41))
 
 order = 4
-#fes = FESpace("trefftzfespace", mesh, flags = { "dgjumps" : True, "order" : 3 })
-fes = FESpace("trefftzfespace", mesh, order=order, testshift = True, basistype=1)
-# fes = FESpace("l22", mesh, order = order, dgjumps = True) #L2(mesh, order=order, flags = { "dgjumps" : True })
+fes = FESpace("trefftzfespace", mesh, order = order, wavespeed = 1, dgjumps=True)
+# fes = FESpace("l2", mesh, order = order, dgjumps = True) #L2(mesh, order=order, flags = { "dgjumps" : True })
 
 u = GridFunction(fes,"shapes")
-
+u.vec[:]=0
 Draw(u)
 
-#def printshape(i):
-#    print("Draw basis function ", i)
-#    u.vec[:] = 0
-#    u.vec[i] = 1
-#    Redraw()
-
-#print("ndof = ", fes.ndof)
-#for i in range(len(u.vec)):
-#    printshape(int(input("enter number of shapefunction to print:")))
-
-
-# we can use the additionally exported function here
-
-# for id in mesh.Elements(VOL):
-# 	fe = fes.GetFE(id)
-
-
-for i in range(2624): #126 #fes.GetNDof()):
+for i in range(u.vec.size):
     print("Draw basis function ", i)
     u.vec[:] = 0
     u.vec[i] = 1
