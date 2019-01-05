6729912418571b83ea26d215b2511eadf0b5bf01
Paul <ping.paul@hotmail.com>
2019-01-05 22:14:16 +0100

    fixed basis matrix return bug, more work to make this part thread safe

--- trefftz/trefftzwavebasis.cpp
+++ trefftz/trefftzwavebasis.cpp
@@ -57,7 +57,7 @@ namespace ngfem
         static Matrix<> trefftzbasis;
 
         //if(tbstore[ord]==NULL)
-        if (trefftzbasis.Height()==0)
+        if (trefftzbasis.Width() != BinCoeff(D + ord, ord))
         {
             const int nbasis = (BinCoeff(D-1 + ord, ord) + BinCoeff(D-1 + ord-1, ord-1));
             const int npoly = (BinCoeff(D + ord, ord));
