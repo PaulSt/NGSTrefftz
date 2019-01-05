7b1160cd1fd026eaa925551ebc66c1b3da6b8157
Paul <ping.paul@hotmail.com>
2019-01-05 22:13:42 +0100

    evolvetent python back to normal

--- python_test/evolvetents.py
+++ python_test/evolvetents.py
@@ -11,12 +11,12 @@ from testcases import *
 order = 4
 c = 1
 t_start = 0
-t_step = 1
+t_step = 0.1
 
 ngmesh = SegMesh(4,0,1)
-# ngmesh = QadSegMesh(4,0,1)
+# ngmesh = QadSegMesh(9,0,1)
 initmesh = Mesh(ngmesh)
-# initmesh = Mesh(unit_square.GenerateMesh(maxh=0.1))
+initmesh = Mesh(unit_square.GenerateMesh(maxh=0.1))
 # initmesh = Mesh(unit_cube.GenerateMesh(maxh = 0.4))
 # initmesh = Mesh( CircleMesh(0.2) )
 # for i in range(0,len(initmesh.GetBoundaries())):
@@ -29,7 +29,6 @@ elif D==1: eltyp = ET.SEGM
 intrule = IntegrationRule(eltyp,2*order)
 irsize = len(intrule.points)
 
-
 fes = H1(initmesh, order=order)
 u,v = fes.TnT()
 gfu = GridFunction(fes)
@@ -39,22 +38,22 @@ a.Assemble()
 Draw(gfu,initmesh,'sol')
 # Draw(gfu,initmesh,'sol',autoscale=False,min=-0.1,max=0.1)
 # Draw(gfu,initmesh,'sol',autoscale=False,min=-0.01,max=0.01)
-bdd = simplesin(D,c)
+bdd = standingwave(D,c)
 wavefront = EvolveTentsMakeWavefront(order,initmesh,c,t_start,bdd)
 
-# for t in range(0,200):
+for t in range(0,200):
 # with TaskManager():
-wavefront = EvolveTents(order,initmesh,c,t_step,wavefront,t_start,bdd)
-print("Error: " + str(EvolveTentsError(order,initmesh,wavefront,EvolveTentsMakeWavefront(order,initmesh,c,t_start + t_step,bdd))))
+    wavefront = EvolveTents(order,initmesh,c,t_step,wavefront,t_start,bdd)
+    print("Error: " + str(EvolveTentsError(order,initmesh,wavefront,EvolveTentsMakeWavefront(order,initmesh,c,t_start + t_step,bdd))))
 
-ipfct=IntegrationPointFunction(initmesh,intrule,wavefront)
-f = LinearForm(fes)
-f += SymbolicLFI(ipfct*v, intrule=intrule)
-f.Assemble()
-gfu.vec.data = a.mat.Inverse(freedofs=fes.FreeDofs()) * f.vec
-Redraw()
+    ipfct=IntegrationPointFunction(initmesh,intrule,wavefront)
+    f = LinearForm(fes)
+    f += SymbolicLFI(ipfct*v, intrule=intrule)
+    f.Assemble()
+    gfu.vec.data = a.mat.Inverse(freedofs=fes.FreeDofs()) * f.vec
+    Redraw()
 
-t_start += t_step
-print("time: " + str(t_start))
+    t_start += t_step
+    print("time: " + str(t_start))
 # filename = "results/mov/sol"+str(t).zfill(3) +".jpg"
 # Tcl_Eval("Ng_SnapShot .ndraw {};\n".format(filename))
