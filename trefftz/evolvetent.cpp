cbc7388833b06c0e043644ac71f94e8a618997d0
Paul <ping.paul@hotmail.com>
2019-01-21 17:21:29 +0100

    different materials working! some cleanup in cpp

--- trefftz/evolvetent.cpp
+++ trefftz/evolvetent.cpp
@@ -148,16 +148,12 @@ namespace ngcomp
                 CalcTentEl<D>(elnr,tent,tel,ma,wavefront,sir,slh,subm,subv);
             }
 
-            //cout << elmat << endl;
-
             // Integrate boundary tent
             for(auto surfel : ma->GetVertexSurfaceElements(tent->vertex))
             {
                 int in;
                 int out;
                 ma->GetSElNeighbouringDomains(surfel, in, out);
-                cout << "tentv: " << tent->vertex << " coord: " << ma->GetPoint<D>(tent->vertex) << endl;
-                cout << "inout " << in << " " << out << endl;
 
                 if(out == 0)
                 {
@@ -245,7 +241,6 @@ namespace ngcomp
             {
                 int eli = ma->GetElIndex(ElementId(VOL,elnr));
                 tel.SetWavespeed(wavespeed[eli]);
-
                 if(ndomains == 1)
                     eli = 0;
                 CalcTentElEval<D>(elnr, tent, tel, ma, wavefront, sir, slh, sol.Range(eli*nbasis,(eli+1)*nbasis));
