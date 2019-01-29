43d1cbdb34689decc0e45aa5df1e55737c0aeeff
Paul <ping.paul@hotmail.com>
2019-01-29 16:53:09 +0100

    playing with parameters is et_L

--- python_test/et_L.py
+++ python_test/et_L.py
@@ -6,33 +6,33 @@ from ngsolve import *
 from prodmesh import *
 from ngsolve.solve import Tcl_Eval # for snapshots
 from testcases import *
+import time
 
 from ngsolve import *
 from netgen.geom2d import unit_square
 from netgen.meshing import MeshingParameters
 
 maxh=0.01
-minh=0.01
+minh=0.005
 mp = MeshingParameters (maxh = maxh)
 refpoints = 500
 for i in range(0, refpoints+1):
     for j in range(0, refpoints+1):
         xk = i/refpoints
         yk = j/refpoints
-        mp.RestrictH (x=xk, y=yk, z=0, h=max(minh,sqrt(0.005*((xk-0.5)*(xk-0.5)+(yk-0.5)*(yk-0.5)))))
+        mp.RestrictH (x=xk, y=yk, z=0, h=max(minh,sqrt(maxh*((xk-0.5)*(xk-0.5)+(yk-0.5)*(yk-0.5)))))
 
 initmesh = Mesh( LshapeMesh(maxh,mp) )
 # RefineAround([0.5,0.5,0],0.1,initmesh)
 # RefineAround([0.5,0.5,0],0.02,initmesh)
 Draw(initmesh)
-input()
 for i in range(0,len(initmesh.GetBoundaries())):
    initmesh.ngmesh.SetBCName(i,"neumann")
 
-order = 2
+order = 3
 c = 1
 t_start = 0
-t_step = 0.05
+t_step = 0.1
 
 D = initmesh.dim
 if D==3: eltyp = ET.TET
@@ -47,28 +47,36 @@ gfu = GridFunction(fes)
 a = BilinearForm(fes)
 a += SymbolicBFI(u*v)
 a.Assemble()
-Draw(gfu,initmesh,'sol')
 # Draw(gfu,initmesh,'sol',autoscale=False,min=-1,max=1)
 bdd = vertgausspw(D,c)
 wavefront = EvolveTentsMakeWavefront(order,initmesh,t_start,bdd )
 # Draw(bdd,initmesh,'sol',autoscale=False,min=-0.05,max=0.1)
-# input()
-# filename = "results/mov/sol"+str(0000)
+# Draw(bdd,initmesh,'sol',autoscale=False,min=-0.05,max=0.1)
+ipfct=IntegrationPointFunction(initmesh,intrule,wavefront)
+f = LinearForm(fes)
+f += SymbolicLFI(ipfct*v, intrule=intrule)
+f.Assemble()
+gfu.vec.data = a.mat.Inverse() * f.vec
+# Draw(gfu,initmesh,'sol')
+Draw(gfu,initmesh,'sol',autoscale=False,min=-0.1,max=0.35)
+input()
+# filename = "results/mov/sol0.jpg"
 # Tcl_Eval("Ng_SnapShot .ndraw {};\n".format(filename))
-# input()
-# Draw(gfu,initmesh,'sol',autoscale=False,min=-0.05,max=0.1)
 
-for t in range(0,100):
-    wavefront = EvolveTents(order,initmesh,c,t_step,wavefront,t_start, bdd )
+runtime = time.time()
+with TaskManager():
+    for t in range(0,10):
+        wavefront = EvolveTents(order,initmesh,c,t_step,wavefront,t_start, bdd )
 
-    ipfct=IntegrationPointFunction(initmesh,intrule,wavefront)
-    f = LinearForm(fes)
-    f += SymbolicLFI(ipfct*v, intrule=intrule)
-    f.Assemble()
-    gfu.vec.data = a.mat.Inverse() * f.vec
-    Redraw()
+        ipfct=IntegrationPointFunction(initmesh,intrule,wavefront)
+        f = LinearForm(fes)
+        f += SymbolicLFI(ipfct*v, intrule=intrule)
+        f.Assemble()
+        gfu.vec.data = a.mat.Inverse() * f.vec
+        Redraw(blocking=True)
 
-    t_start += t_step
-    print("time: " + str(t_start))
-    filename = "results/mov/sol"+str(t).zfill(3)
-    Tcl_Eval("Ng_SnapShot .ndraw {};\n".format(filename))
+        t_start += t_step
+        print("time: " + str(t_start))
+        # filename = "results/mov/sol"+str(t).zfill(3)+".jpg"
+        # Tcl_Eval("Ng_SnapShot .ndraw {};\n".format(filename))
+print("\n",time.time()-runtime)
