4e474eee1952588145f28c6f1e4a1e68580cdade
Paul <ping.paul@hotmail.com>
2019-01-29 16:56:17 +0100

    updated timestepping.py

--- python_test/timestepping.py
+++ python_test/timestepping.py
@@ -4,14 +4,15 @@ from math import pi
 from ngsolve import *
 from netgen.geom2d import SplineGeometry
 import numpy as np
+import time
 
 from prodmesh import *
 from netgen.geom2d import unit_square
 from netgen.meshing import MeshingParameters
 
 mesh = Mesh(unit_square.GenerateMesh(maxh=0.08))
-maxh=0.01
-minh=0.01
+maxh=0.015
+minh=0.005
 mp = MeshingParameters (maxh = maxh)
 refpoints = 500
 for i in range(0, refpoints+1):
@@ -26,7 +27,7 @@ fes = H1(mesh, order=2) #, dirichlet="bottom|right|left|top")
 
 u,v = fes.TnT() # TnT : Trial and Test function
 
-dt = 0.001
+dt = 0.0008
 
 a = BilinearForm(fes)
 a += SymbolicBFI (grad(u)*grad(v))
@@ -54,20 +55,22 @@ u1.Set( exp(-delta*((x-0.25)*(x-0.25))) )
 Draw(u1,mesh,"u")
 input()
 
-# time =  0.0
-time = dt
+ts = dt
 res = u0.vec.CreateVector()
-while time < 1:
-    time += dt
-    # uD.Set(sin(time+sq*(x+y)),BND)
-    res.data = dt*dt * (- a.mat * u1.vec  )
-    u2.vec.data = 2*u1.vec - u0.vec + invm * res
-    # for i,t in enumerate(fes.FreeDofs()):
-        # if t==False:
-            # u2.vec[i] = uD.vec[i]
-    u0.vec.data = u1.vec.data
-    u1.vec.data = u2.vec.data
+runtime = time.time()
+with TaskManager():
+    while ts < 1:
+        ts += dt
+        # uD.Set(sin(ts+sq*(x+y)),BND)
+        res.data = dt*dt * (- a.mat * u1.vec  )
+        u2.vec.data = 2*u1.vec - u0.vec + invm * res
+        # for i,t in enumerate(fes.FreeDofs()):
+            # if t==False:
+                # u2.vec[i] = uD.vec[i]
+        u0.vec.data = u1.vec.data
+        u1.vec.data = u2.vec.data
 
-    if(int(1000*time)%100==0):
-        Redraw(blocking=True)
-    print("\r",time,"   ",time%0.1,end="")
+        if(int(1000*ts)%100==0):
+            Redraw(blocking=True)
+            print("\r",ts,end="")
+print("\n",time.time()-runtime)
