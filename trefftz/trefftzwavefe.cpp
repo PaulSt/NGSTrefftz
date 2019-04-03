f9afb6b0f818c52ef28c9c649fa3c0d0f78a3293
Paul <ping.paul@hotmail.com>
2019-04-03 17:09:36 +0200

    working on slab vs tents

--- trefftz/trefftzwavefe.cpp
+++ trefftz/trefftzwavefe.cpp
@@ -293,7 +293,8 @@ namespace ngfem
         cpoint -= elcenter; cpoint *= (2.0/elsize); cpoint[2] *= c;
         // calc 1 dimensional monomial basis
         STACK_ARRAY(double, mem, 3*(ord+1));
-        double* polxt[3];
+        //double* polxt[3];
+        Vec<3,double*> polxt;
         for(size_t d=0;d<3;d++)
         {
             polxt[d] = &mem[d*(ord+1)];
@@ -351,7 +352,7 @@ namespace ngfem
     template<>
     void TrefftzWaveFE<1> :: CalcDShape (const BaseMappedIntegrationPoint & mip,
                                          SliceMatrix<> dshape) const
-    {}
+    {cout << "dim not implemented" << endl;}
 
     template<>
     void TrefftzWaveFE<2> :: CalcDShape (const BaseMappedIntegrationPoint & mip,
@@ -395,8 +396,9 @@ namespace ngfem
         Vec<3> cpoint = mip.GetPoint();
         cpoint -= elcenter; cpoint *= (2.0/elsize); cpoint[2] *= c;
         // +1 size to avoid undefined behavior taking deriv, getting [-1] entry
-        STACK_ARRAY(double, mem, 3*(ord+1)+1);
-        double* polxt[4];
+        STACK_ARRAY(double, mem, 3*(ord+1)+2);
+        //double* polxt[4];
+        Vec<3,double*> polxt;
         for(size_t d=0;d<3;d++)
         {
             polxt[d] = &mem[d*(ord+1)+1];
