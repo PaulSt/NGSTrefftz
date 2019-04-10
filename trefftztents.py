e89bf566fb36405888b262106771c31390114887
Paul <ping.paul@hotmail.com>
2019-04-10 09:51:53 +0200

    bach to wavespeed+1, add some other examples

--- trefftztents.py
+++ trefftztents.py
@@ -11,12 +11,13 @@ t_start = 0
 t_step = 2/sqrt(3)
 
 initmesh = Mesh(unit_cube.GenerateMesh(maxh = 0.2))
+# for i in range(0,len(initmesh.GetBoundaries())):
+   # initmesh.ngmesh.SetBCName(i,"neumann")
 
 D = initmesh.dim
-
-c = 1
-sq = sqrt(3.0);
 t = CoordCF(D)
+
+sq = sqrt(3.0);
 bdd = CoefficientFunction((
     sin(math.pi*x)*sin(math.pi*y)*sin(math.pi*z)*sin(math.pi*t*c*sq)/(sq*math.pi),
     cos(math.pi*x)*sin(math.pi*y)*sin(math.pi*z)*sin(math.pi*t*c*sq)/sq,
@@ -24,12 +25,34 @@ bdd = CoefficientFunction((
     sin(math.pi*x)*sin(math.pi*y)*cos(math.pi*z)*sin(math.pi*t*c*sq)/sq,
     sin(math.pi*x)*sin(math.pi*y)*sin(math.pi*z)*cos(math.pi*t*c*sq)*c
     ))
+# sq = sqrt(1.0/3.0);
+# bdd = CoefficientFunction((
+    # sin( c*t+sq*(x+y+z) ),
+    # sq*cos(c*t+sq*(x+y+z)),
+    # sq*cos(c*t+sq*(x+y+z)),
+    # sq*cos(c*t+sq*(x+y+z)),
+    # c*cos(c*t+sq*(x+y+z))
+    # ))
+# bdd = CoefficientFunction((
+    # sin(sq*x)*sin(sq*y)*sin(sq*z)*sin(t*c),
+    # sq*cos(sq*x)*sin(sq*y)*sin(sq*z)*sin(t*c),
+    # sq*sin(sq*x)*cos(sq*y)*sin(sq*z)*sin(t*c),
+    # sq*sin(sq*x)*sin(sq*y)*cos(sq*z)*sin(t*c),
+    # sin(sq*x)*sin(sq*y)*sin(sq*z)*cos(t*c)*c
+    # ))
+# bdd = CoefficientFunction((
+# cos(math.pi*x)*cos(math.pi*y)*cos(math.pi*z)*sin(math.pi*t*c*sq)/(sq*math.pi),
+# -sin(math.pi*x)*cos(math.pi*y)*cos(math.pi*z)*sin(math.pi*t*c*sq)/sq,
+# -cos(math.pi*x)*sin(math.pi*y)*cos(math.pi*z)*sin(math.pi*t*c*sq)/sq,
+# -cos(math.pi*x)*cos(math.pi*y)*sin(math.pi*z)*sin(math.pi*t*c*sq)/sq,
+# cos(math.pi*x)*cos(math.pi*y)*cos(math.pi*z)*cos(math.pi*t*c*sq)*c
+# ))
 wavefront = EvolveTentsMakeWavefront(order,initmesh,t_start,bdd)
 
 start = time.time()
-with TaskManager():
 # SetNumThreads(1)
 # with TaskManager(pajetrace=100*1000*1000):
+with TaskManager():
     wavefront = EvolveTents(order,initmesh,c,t_step,wavefront,t_start,bdd)
 print("time ",time.time()-start)
 error = EvolveTentsError(order,initmesh,wavefront,EvolveTentsMakeWavefront(order,initmesh,t_start + t_step,bdd))
