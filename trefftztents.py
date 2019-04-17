8bfa3f486c1528f35748923b1ed6a377f989d1f2
Paul <stocker.paul@hotmail.com>
2019-04-17 14:47:47 +0200

    cleanup trefftztents

--- trefftztents.py
+++ trefftztents.py
@@ -4,21 +4,26 @@ from netgen.csg import unit_cube
 import time
 import math
 from trefftzngs import *
-SetHeapSize(1000*1000*1000)
+# SetHeapSize(1000*1000*1000)
 
-order = 4
+order = 3
+SetNumThreads(12)
 
 c = 1
 t_start = 0
 t_step = 2/sqrt(3)
 
 h1error = []
-ms = [0.25,0.2,0.15,0.13]
+adiam = []
+timer = []
+# ms = [0.4,0.3,0.2,0.1]
+ms = [0,1,2,3,4]
 
+initmesh = Mesh(unit_cube.GenerateMesh(maxh = 0.5))
 for maxh in ms:
-    initmesh = Mesh(unit_cube.GenerateMesh(maxh = maxh))
-# for i in range(0,len(initmesh.GetBoundaries())):
-       # initmesh.ngmesh.SetBCName(i,"neumann")
+    print("RUN: ", maxh)
+    # initmesh = Mesh(unit_cube.GenerateMesh(maxh = maxh))
+    # initmesh = Mesh(unit_square.GenerateMesh(maxh = maxh))
 
     D = initmesh.dim
     t = CoordCF(D)
@@ -31,28 +36,6 @@ for maxh in ms:
         sin(math.pi*x)*sin(math.pi*y)*cos(math.pi*z)*sin(math.pi*t*c*sq)/sq,
         sin(math.pi*x)*sin(math.pi*y)*sin(math.pi*z)*cos(math.pi*t*c*sq)*c
         ))
-# sq = sqrt(1.0/3.0);
-# bdd = CoefficientFunction((
-        # sin( c*t+sq*(x+y+z) ),
-        # sq*cos(c*t+sq*(x+y+z)),
-        # sq*cos(c*t+sq*(x+y+z)),
-        # sq*cos(c*t+sq*(x+y+z)),
-        # c*cos(c*t+sq*(x+y+z))
-        # ))
-# bdd = CoefficientFunction((
-        # sin(sq*x)*sin(sq*y)*sin(sq*z)*sin(t*c),
-        # sq*cos(sq*x)*sin(sq*y)*sin(sq*z)*sin(t*c),
-        # sq*sin(sq*x)*cos(sq*y)*sin(sq*z)*sin(t*c),
-        # sq*sin(sq*x)*sin(sq*y)*cos(sq*z)*sin(t*c),
-        # sin(sq*x)*sin(sq*y)*sin(sq*z)*cos(t*c)*c
-        # ))
-# bdd = CoefficientFunction((
-# cos(math.pi*x)*cos(math.pi*y)*cos(math.pi*z)*sin(math.pi*t*c*sq)/(sq*math.pi),
-# -sin(math.pi*x)*cos(math.pi*y)*cos(math.pi*z)*sin(math.pi*t*c*sq)/sq,
-# -cos(math.pi*x)*sin(math.pi*y)*cos(math.pi*z)*sin(math.pi*t*c*sq)/sq,
-# -cos(math.pi*x)*cos(math.pi*y)*sin(math.pi*z)*sin(math.pi*t*c*sq)/sq,
-# cos(math.pi*x)*cos(math.pi*y)*cos(math.pi*z)*cos(math.pi*t*c*sq)*c
-# ))
     wavefront = EvolveTentsMakeWavefront(order,initmesh,t_start,bdd)
 
     fes = H1(initmesh, order=order)
@@ -65,39 +48,25 @@ for maxh in ms:
     intrule = IntegrationRule(eltyp,2*order)
     irsize = len(intrule.points)
 
-
     start = time.time()
-    SetNumThreads(8)
-# with TaskManager(pajetrace=100*1000*1000):
     with TaskManager():
         wavefront = EvolveTents(order,initmesh,c,t_step,wavefront,t_start,bdd)
+    timer.append(time.time()-start)
     print("time ",time.time()-start)
     error = EvolveTentsError(order,initmesh,wavefront,EvolveTentsMakeWavefront(order,initmesh,t_step,bdd))
     h1error.append(error)
     print("error ", error)
-
-    ipfct=IntegrationPointFunction(initmesh,intrule,wavefront)
-    f = LinearForm(fes)
-    f += SymbolicLFI(ipfct*v, intrule=intrule)
-    f.Assemble()
-    gfu.vec.data = a.mat.Inverse(freedofs=fes.FreeDofs()) * f.vec
-
-    # print(sqrt(Integrate(( sin(math.pi*x)*sin(math.pi*y)*sin(math.pi*z)*sin(math.pi*t_step*c*sq)/(sq*math.pi)-gfu)*( sin(math.pi*x)*sin(math.pi*y)*sin(math.pi*z)*sin(math.pi*t_step*c*sq)/(sq*math.pi)-gfu),initmesh)))
-
-    # corsol= CoefficientFunction((cos(math.pi*x)*sin(math.pi*y)*sin(math.pi*z)*sin(math.pi*t_step*c*sq)/sq,
-        # sin(math.pi*x)*cos(math.pi*y)*sin(math.pi*z)*sin(math.pi*t_step*c*sq)/sq,
-        # sin(math.pi*x)*sin(math.pi*y)*cos(math.pi*z)*sin(math.pi*t_step*c*sq)/sq))
-    # print(sqrt(Integrate(( corsol-grad(gfu))*( corsol-grad(gfu)),initmesh)))
     adiam.append(EvolveTentsAdiam(initmesh,1,t_step))
 
-    initmesh.Refine()
+    if maxh != ms[-1]:
+        initmesh.Refine()
 
 print("rate")
 from math import *
 for i in range(len(ms)-1):
-    print(log(h1error[i]/h1error[i+1])/log(1/2))
+    print(log(h1error[i]/h1error[i+1])/log(2))
     # print(log(h1error[i]/h1error[i+1])/log(ms[i]/ms[i+1]))
 for i in range(len(ms)):
-    print("adiam ",adiam[i]," maxh ", ms[i]," error ", h1error[i])
+    print("adiam ",adiam[i]," maxh ", ms[i]," error ", h1error[i], " time ", timer[i])
 for i in range(len(ms)-1):
     print(log(h1error[i]/h1error[i+1])/log(adiam[i]/adiam[i+1]))
