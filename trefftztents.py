72e8a08ce542fab4a28114357110cfd801d989cc
Paul <stocker.paul@hotmail.com>
2019-04-19 09:50:40 +0200

    function in trefftztents.py

--- trefftztents.py
+++ trefftztents.py
@@ -6,25 +6,8 @@ import math
 from trefftzngs import *
 # SetHeapSize(1000*1000*1000)
 
-order = 3
-SetNumThreads(12)
 
-c = 1
-t_start = 0
-t_step = 2/sqrt(3)
-
-h1error = []
-adiam = []
-timer = []
-# ms = [0.4,0.3,0.2,0.1]
-ms = [0,1,2,3,4]
-
-initmesh = Mesh(unit_cube.GenerateMesh(maxh = 0.5))
-for maxh in ms:
-    print("RUN: ", maxh)
-    # initmesh = Mesh(unit_cube.GenerateMesh(maxh = maxh))
-    # initmesh = Mesh(unit_square.GenerateMesh(maxh = maxh))
-
+def SolveTrefftzTents(mesh, order, finaltime):
     D = initmesh.dim
     t = CoordCF(D)
 
@@ -38,35 +21,51 @@ for maxh in ms:
         ))
     wavefront = EvolveTentsMakeWavefront(order,initmesh,t_start,bdd)
 
-    fes = H1(initmesh, order=order)
-    u,v = fes.TnT()
-    gfu = GridFunction(fes)
-    a = BilinearForm(fes)
-    a += SymbolicBFI(u*v)
-    a.Assemble()
-    eltyp = ET.TET
-    intrule = IntegrationRule(eltyp,2*order)
-    irsize = len(intrule.points)
-
     start = time.time()
     with TaskManager():
         wavefront = EvolveTents(order,initmesh,c,t_step,wavefront,t_start,bdd)
-    timer.append(time.time()-start)
+    timing = (time.time()-start)
     print("time ",time.time()-start)
     error = EvolveTentsError(order,initmesh,wavefront,EvolveTentsMakeWavefront(order,initmesh,t_step,bdd))
-    h1error.append(error)
     print("error ", error)
-    adiam.append(EvolveTentsAdiam(initmesh,1,t_step))
+    adiam = EvolveTentsAdiam(initmesh,1,t_step)
 
-    if maxh != ms[-1]:
-        initmesh.Refine()
+    return [error, timing, adiam]
 
-print("rate")
-from math import *
-for i in range(len(ms)-1):
-    print(log(h1error[i]/h1error[i+1])/log(2))
-    # print(log(h1error[i]/h1error[i+1])/log(ms[i]/ms[i+1]))
-for i in range(len(ms)):
-    print("adiam ",adiam[i]," maxh ", ms[i]," error ", h1error[i], " time ", timer[i])
-for i in range(len(ms)-1):
-    print(log(h1error[i]/h1error[i+1])/log(adiam[i]/adiam[i+1]))
+
+if __name__ == '__main__':
+    order = 3
+    SetNumThreads(10)
+
+    c = 1
+    t_start = 0
+    t_step = 2/sqrt(3)
+
+    h1error = []
+    adiam = []
+    timer = []
+# ms = [0.4,0.3,0.2,0.1]
+    ms = [0,1,2,3,4,5]
+
+    initmesh = Mesh(unit_cube.GenerateMesh(maxh = 1))
+    for maxh in ms:
+        print("RUN: ", maxh)
+        # initmesh = Mesh(unit_cube.GenerateMesh(maxh = maxh))
+        # initmesh = Mesh(unit_square.GenerateMesh(maxh = maxh))
+        [error,timing,tentdiam] =  SolveTrefftzTents(initmesh, order, t_step)
+        h1error.append(error)
+        adiam.append(tentdiam)
+        timer.append(timing)
+
+        if maxh != ms[-1]:
+            initmesh.Refine()
+
+    print("rate")
+    from math import *
+    for i in range(len(ms)-1):
+        print(log(h1error[i]/h1error[i+1])/log(2))
+        # print(log(h1error[i]/h1error[i+1])/log(ms[i]/ms[i+1]))
+    for i in range(len(ms)):
+        print("adiam ",adiam[i]," maxh ", ms[i]," error ", h1error[i], " time ", timer[i])
+    for i in range(len(ms)-1):
+        print(log(h1error[i]/h1error[i+1])/log(adiam[i]/adiam[i+1]))
