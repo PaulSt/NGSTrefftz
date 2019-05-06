a62c01f52042c355bebbb72358a0835d7e9ae6bd
Paul <ping.paul@hotmail.com>
2019-05-06 10:06:55 +0200

    add SIMD functions to trefftzwavefe

--- trefftz/trefftzwavefe.hpp
+++ trefftz/trefftzwavefe.hpp
@@ -29,11 +29,63 @@ namespace ngfem
 
             using ScalarMappedElement<D>::CalcShape;
             virtual void CalcShape (const BaseMappedIntegrationPoint & mip, BareSliceVector<> shape) const;
-            virtual void CalcShape (const SIMD_MappedIntegrationRule<D-1,D> & smir, BareSliceMatrix<SIMD<double>> shape) const;
+            virtual void CalcShape (const SIMD_BaseMappedIntegrationRule & smir, BareSliceMatrix<SIMD<double>> shape) const;
 
             using ScalarMappedElement<D>::CalcDShape;
             virtual void CalcDShape (const BaseMappedIntegrationPoint & mip, SliceMatrix<> dshape) const;
-            virtual void CalcDShape (const SIMD_MappedIntegrationRule<D-1,D> & smir, SliceMatrix<SIMD<double>> dshape) const;
+            virtual void CalcDShape (const SIMD_BaseMappedIntegrationRule & smir, BareSliceMatrix<SIMD<double>> dshape) const;
+
+
+    void Evaluate (const SIMD_BaseMappedIntegrationRule & mir, BareSliceVector<> coefs, BareVector<SIMD<double>> values) const
+    {
+        STACK_ARRAY(SIMD<double>, mem, nbasis*mir.Size());
+        FlatMatrix<SIMD<double>> shape(nbasis,mir.Size(),&mem[0]);
+        CalcShape (mir, shape);
+        const int nsimd = SIMD<double>::Size();
+        FlatMatrix<double> bdbmat(nbasis,mir.Size()*nsimd,&shape(0,0)[0]);
+        FlatVector<double> bdbvec(mir.Size()*nsimd,&values(0)[0]);
+        bdbvec = Trans(bdbmat) * coefs;
+    }
+    void AddTrans (const SIMD_BaseMappedIntegrationRule & mir, BareVector<SIMD<double>> values, BareSliceVector<> coefs) const
+    {
+        STACK_ARRAY(SIMD<double>, mem, nbasis*mir.Size());
+        FlatMatrix<SIMD<double>> shape(nbasis,mir.Size(),&mem[0]);
+        CalcShape (mir, shape);
+        const int nsimd = SIMD<double>::Size();
+        FlatMatrix<double> bdbmat(nbasis,mir.Size()*nsimd,&shape(0,0)[0]);
+        FlatVector<double> bdbvec(mir.Size()*nsimd,&values(0)[0]);
+        coefs.AddSize(nbasis) += bdbmat * bdbvec;
+    }
+
+    //using ScalarMappedElement<D>::CalcMappedDShape;
+    //HD NGS_DLL_HEADER virtual
+    void CalcMappedDShape (const SIMD_BaseMappedIntegrationRule & mir,
+                      BareSliceMatrix<SIMD<double>> dshapes) const
+    {
+        CalcDShape(mir,dshapes);
+    }
+    void EvaluateGrad (const SIMD_BaseMappedIntegrationRule & ir, BareSliceVector<> coefs, BareSliceMatrix<SIMD<double>> values) const
+    {
+        STACK_ARRAY(SIMD<double>, mem, (D)*nbasis*ir.Size());
+        FlatMatrix<SIMD<double>> simddshapes((D)*nbasis,ir.Size(),&mem[0]);
+        CalcDShape(ir,simddshapes);
+        const int nsimd = SIMD<double>::Size();
+        FlatMatrix<double> dshapes(nbasis,(D)*nsimd*ir.Size(),&simddshapes(0,0)[0]);
+        FlatVector<double> bdbvec((D)*nsimd*ir.Size(),&values(0,0)[0]);
+        bdbvec = Trans(dshapes)*coefs;
+    }
+    void AddGradTrans (const SIMD_BaseMappedIntegrationRule & mir, BareSliceMatrix<SIMD<double>> values,
+                  BareSliceVector<> coefs) const
+    {
+        STACK_ARRAY(SIMD<double>, mem, (D)*nbasis*mir.Size());
+        FlatMatrix<SIMD<double>> simddshapes((D)*nbasis,mir.Size(),&mem[0]);
+        CalcDShape(mir,simddshapes);
+        const int nsimd = SIMD<double>::Size();
+        FlatMatrix<double> dshapes(nbasis,(D)*nsimd*mir.Size(),&simddshapes(0,0)[0]);
+        FlatVector<double> bdbvec((D)*nsimd*mir.Size(),&values(0,0)[0]);
+        coefs.AddSize(nbasis) += dshapes*bdbvec;
+    }
+
 
             int GetNBasis() const { return nbasis; }
             float GetWavespeed() const { return c; }
