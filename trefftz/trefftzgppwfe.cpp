751bd7ccef6fc9c25487445d94f3cc67d68c15b4
Paul <stocker.paul@hotmail.com>
2019-11-07 10:09:40 +0100

    gppw update, basis working(?)

--- trefftz/trefftzgppwfe.cpp
+++ trefftz/trefftzgppwfe.cpp
@@ -8,12 +8,14 @@
 namespace ngfem
 {
   template <int D>
-  TrefftzGppwFE<D>::TrefftzGppwFE (int aord, float ac, Vec<D> aelcenter,
+  TrefftzGppwFE<D>::TrefftzGppwFE (const Array<double> &agamma, int agppword,
+                                   int aord, float ac, Vec<D> aelcenter,
                                    double aelsize, ELEMENT_TYPE aeltype,
                                    int abasistype)
       : ScalarMappedElement<D + 1> (2 * aord + 1, aord), ord (aord), c (ac),
-        npoly (BinCoeff (D + ord, ord)), elcenter (aelcenter),
-        elsize (aelsize), eltype (aeltype), basistype (abasistype)
+        npoly (BinCoeff (D + 1 + ord, ord)), elcenter (aelcenter),
+        elsize (aelsize), eltype (aeltype), basistype (abasistype),
+        gppword (agppword), gamma (agamma)
   {
     ;
   }
@@ -22,6 +24,7 @@ namespace ngfem
   void TrefftzGppwFE<1>::CalcShape (const SIMD_BaseMappedIntegrationRule &smir,
                                     BareSliceMatrix<SIMD<double>> shape) const
   {
+    throw ExceptionNOSIMD ("SIMD - CalcShape not overloaded");
     for (int imip = 0; imip < smir.Size (); imip++)
       {
         Vec<2, SIMD<double>> cpoint = smir[imip].GetPoint ();
@@ -34,6 +37,30 @@ namespace ngfem
           for (int d = 0; d < NDirections (i); ++d)
             shape (basisn++, imip)
                 = pow (GetDirection (i, d) * cpoint[0] - cpoint[1], i);
+
+        // calc 1 dimensional monomial basis
+        STACK_ARRAY (SIMD<double>, mem, 2 * (gppword + 1));
+        int npoly = BinCoeff (1 + 1 + gppword, gppword);
+        Vec<2, SIMD<double> *> polxt;
+        for (size_t d = 0; d < 2; d++)
+          {
+            polxt[d] = &mem[d * (gppword + 1)];
+            Monomial (gppword, cpoint[d], polxt[d]);
+          }
+        // calc D+1 dimenional monomial basis
+        Vector<SIMD<double>> pol (npoly);
+        for (size_t i = 0, ii = 0; i <= gppword; i++)
+          for (size_t j = 0; j <= gppword - i; j++)
+            pol[ii++] = polxt[0][i] * polxt[1][j];
+        // TB*monomials for trefftz shape fcts
+        const CSR *localmat
+            = TrefftzGppwBasis<1>::getInstance ().TB (ord, gppword, gamma);
+        for (int i = 0; i < this->ndof; ++i)
+          {
+            // shape(i,imip) = 0.0;
+            for (int j = (*localmat)[0][i]; j < (*localmat)[0][i + 1]; ++j)
+              shape (i, imip) += (*localmat)[2][j] * pol[(*localmat)[1][j]];
+          }
       }
   }
 
@@ -48,6 +75,7 @@ namespace ngfem
   void TrefftzGppwFE<3>::CalcShape (const SIMD_BaseMappedIntegrationRule &smir,
                                     BareSliceMatrix<SIMD<double>> shape) const
   {
+    cout << "dim not implemented" << endl;
   }
 
   template <>
@@ -55,6 +83,7 @@ namespace ngfem
   TrefftzGppwFE<1>::CalcDShape (const SIMD_BaseMappedIntegrationRule &smir,
                                 BareSliceMatrix<SIMD<double>> dshape) const
   {
+    throw ExceptionNOSIMD ("SIMD - CalcShape not overloaded");
     for (int imip = 0; imip < smir.Size (); imip++)
       {
         Vec<2, SIMD<double>> cpoint = smir[imip].GetPoint ();
@@ -74,6 +103,37 @@ namespace ngfem
                       * (d == 0 ? GetDirection (i, dir) : 1)
                       * (d == 1 ? (-c) : 1) * (2.0 / elsize);
           }
+
+        //+1 size to avoid undefined behavior taking deriv, getting [-1] entry
+        STACK_ARRAY (SIMD<double>, mem, 2 * (gppword + 1) + 1);
+        mem[0] = 0;
+        int npoly = BinCoeff (1 + 1 + gppword, gppword);
+        Vec<2, SIMD<double> *> polxt;
+        for (size_t d = 0; d < 2; d++)
+          {
+            polxt[d] = &mem[d * (gppword + 1) + 1];
+            Monomial (gppword, cpoint[d], polxt[d]);
+          }
+
+        for (int d = 0; d < 2; d++)
+          {
+            Vector<SIMD<double>> pol (npoly);
+            for (size_t i = 0, ii = 0; i <= gppword; i++)
+              for (size_t j = 0; j <= gppword - i; j++)
+                pol[ii++] = (d == 0 ? i : (d == 1 ? j : 0))
+                            * polxt[0][i - (d == 0)] * polxt[1][j - (d == 1)];
+
+            const CSR *localmat
+                = TrefftzGppwBasis<1>::getInstance ().TB (ord, gppword, gamma);
+            for (int i = 0; i < this->ndof; ++i)
+              {
+                // dshape(i*2+d,imip) = 0.0;
+                for (int j = (*localmat)[0][i]; j < (*localmat)[0][i + 1]; ++j)
+                  dshape (i * 2 + d, imip)
+                      += (*localmat)[2][j] * pol[(*localmat)[1][j]]
+                         * (d == 1 ? c : 1) * (2.0 / elsize);
+              }
+          }
       }
   }
 
@@ -82,6 +142,7 @@ namespace ngfem
   TrefftzGppwFE<2>::CalcDShape (const SIMD_BaseMappedIntegrationRule &smir,
                                 BareSliceMatrix<SIMD<double>> dshape) const
   {
+    cout << "dim not implemented" << endl;
   }
 
   template <>
@@ -89,6 +150,7 @@ namespace ngfem
   TrefftzGppwFE<3>::CalcDShape (const SIMD_BaseMappedIntegrationRule &smir,
                                 BareSliceMatrix<SIMD<double>> dshape) const
   {
+    cout << "dim not implemented" << endl;
   }
 
   /////////////// non-simd
@@ -97,115 +159,249 @@ namespace ngfem
   void TrefftzGppwFE<1>::CalcShape (const BaseMappedIntegrationPoint &mip,
                                     BareSliceVector<> shape) const
   {
-    cout << "dim not implemented" << endl;
+    // Vec<2,double> cpoint = mip.GetPoint();
+    // cpoint -= elcenter; cpoint *= (2.0/elsize); cpoint[1] *= c;
+    //// calc 1 dimensional monomial basis
+    // STACK_ARRAY(double, mem, 2*(ord+1)+1);
+    // for(size_t d=0;d<2;d++)
+    //{
+    // double evalpoint = pow(-1,d)*cpoint[0]-cpoint[1];
+    // Monomial (ord, evalpoint, &mem[d*(ord+1)]);
+    //}
+    // for (int i=0; i<this->ndof; ++i)
+    //{
+    // shape(i) = i<=ord ? mem[i] : mem[i+1];
+    //}
+    // shape(0)=1;
+    // shape(1)=cpoint[0]-c*cpoint[1];
+    // shape(2)=-cpoint[0]-c*cpoint[1];
+    // int basisn=3;
+    // for (int i=2; i<=ord;i++)
+    // for(int d=0;d<NDirections(i);d++)
+    // shape(basisn++)=shape(basisn-2)*((d%2?-1:1)*cpoint[0]-c*cpoint[1]);
+    Vec<2> cpoint = mip.GetPoint ();
+    cpoint -= elcenter;
+    cpoint *= (2.0 / elsize);
+    // calc 1 dimensional monomial basis
+    for (int i = 0, basisn = 0; i <= ord; ++i)
+      for (int d = 0; d < NDirections (i); ++d)
+        shape (basisn++)
+            = pow (GetDirection (i, d) * cpoint[0] - c * cpoint[1], i);
+
+    // calc 1 dimensional monomial basis
+    STACK_ARRAY (double, mem, 2 * (gppword + 1));
+    int npoly = BinCoeff (1 + 1 + gppword, gppword);
+    double *polxt[2];
+    for (size_t d = 0; d < 2; d++)
+      {
+        polxt[d] = &mem[d * (gppword + 1)];
+        Monomial (gppword, cpoint[d], polxt[d]);
+      }
+    // calc D+1 dimenional monomial basis
+    Vector<double> pol (npoly);
+    for (size_t i = 0, ii = 0; i <= gppword; i++)
+      for (size_t j = 0; j <= gppword - i; j++)
+        pol[ii++] = polxt[0][i] * polxt[1][j];
+    // TB*monomials for trefftz shape fcts
+    const CSR *localmat
+        = TrefftzGppwBasis<1>::getInstance ().TB (ord, gppword, gamma);
+    for (int i = 0; i < this->ndof; ++i)
+      {
+        // shape(i) = 0.0;
+        for (int j = (*localmat)[0][i]; j < (*localmat)[0][i + 1]; ++j)
+          shape (i) += (*localmat)[2][j] * pol[(*localmat)[1][j]];
+      }
   }
 
   template <>
   void TrefftzGppwFE<2>::CalcShape (const BaseMappedIntegrationPoint &mip,
                                     BareSliceVector<> shape) const
   {
+    cout << "dim not implemented" << endl;
   }
 
   template <>
   void TrefftzGppwFE<3>::CalcShape (const BaseMappedIntegrationPoint &mip,
                                     BareSliceVector<> shape) const
   {
+    cout << "dim not implemented" << endl;
   }
 
   template <>
   void TrefftzGppwFE<1>::CalcDShape (const BaseMappedIntegrationPoint &mip,
                                      SliceMatrix<> dshape) const
   {
-    cout << "dim not implemented" << endl;
+    Vec<2> cpoint = mip.GetPoint ();
+    cpoint -= elcenter;
+    cpoint *= (2.0 / elsize);
+    // calc 1 dimensional monomial basis
+    for (int d = 0; d < 2; d++)
+      {
+        for (int i = 0, basisn = 0; i <= ord; ++i)
+          for (int dir = 0; dir < NDirections (i); ++dir)
+            dshape (basisn++, d)
+                = i
+                  * pow (GetDirection (i, dir) * cpoint[0] - c * cpoint[1],
+                         (i - 1) * (i > 0))
+                  * (d == 0 ? GetDirection (i, dir) : 1) * (d == 1 ? (-c) : 1)
+                  * (2.0 / elsize);
+      }
+
+    // +1 size to avoid undefined behavior taking deriv, getting [-1] entry
+    STACK_ARRAY (double, mem, 2 * (gppword + 1) + 1);
+    mem[0] = 0;
+    int npoly = BinCoeff (1 + 1 + gppword, gppword);
+    double *polxt[2];
+    for (size_t d = 0; d < 2; d++)
+      {
+        polxt[d] = &mem[d * (gppword + 1) + 1];
+        Monomial (gppword, cpoint[d], polxt[d]);
+      }
+
+    for (int d = 0; d < 2; d++)
+      {
+        Vector<double> pol (npoly);
+        for (size_t i = 0, ii = 0; i <= gppword; i++)
+          for (size_t j = 0; j <= gppword - i; j++)
+            pol[ii++] = (d == 0 ? i : (d == 1 ? j : 0))
+                        * polxt[0][i - (d == 0)] * polxt[1][j - (d == 1)];
+
+        const CSR *localmat
+            = TrefftzGppwBasis<1>::getInstance ().TB (ord, gppword, gamma);
+        for (int i = 0; i < this->ndof; ++i)
+          {
+            // dshape(i,d) = 0.0;
+            for (int j = (*localmat)[0][i]; j < (*localmat)[0][i + 1]; ++j)
+              dshape (i, d) += (*localmat)[2][j] * pol[(*localmat)[1][j]]
+                               * (2.0 / elsize);
+          }
+      }
   }
 
   template <>
   void TrefftzGppwFE<2>::CalcDShape (const BaseMappedIntegrationPoint &mip,
                                      SliceMatrix<> dshape) const
   {
+    cout << "dim not implemented" << endl;
   }
 
   template <>
   void TrefftzGppwFE<3>::CalcDShape (const BaseMappedIntegrationPoint &mip,
                                      SliceMatrix<> dshape) const
   {
+    cout << "dim not implemented" << endl;
   }
 
   template class TrefftzGppwFE<1>;
   template class TrefftzGppwFE<2>;
   template class TrefftzGppwFE<3>;
 
-  template <int D> const CSR *TrefftzGppwBasis<D>::TB (int ord)
+  template <int D>
+  const CSR *
+  TrefftzGppwBasis<D>::TB (int ord, int gppword, const Array<double> &gamma,
+                           int basistype)
   {
-    const CSR *tb = &tbstore[ord];
+    {
+      lock_guard<mutex> lock (gentrefftzbasis);
+      if (gtbstore.Size () < ord)
+        {
+          cout << "creating gppw bstore for order " << ord << endl;
+          int oldsize = gtbstore.Size ();
+          gtbstore.SetSize (ord + 1);
+          for (int i = oldsize; i <= ord; i++)
+            gtbstore[i] = CSR ();
+
+          if (gtbstore[ord][0].Size () == 0)
+            {
+              const int nbasis = 2 * ord + 1;
+              const int npoly = BinCoeff (D + 1 + gppword, gppword);
+              Matrix<> trefftzbasis (nbasis, npoly);
+              trefftzbasis = 0;
+              Vec<D + 1, int> coeff = 0;
+              int count = 0;
+              int basisn = 0;
+              for (int j = 0; j <= ord; ++j)
+                {
+                  for (int dir = 0; dir < TrefftzGppwFE<D>::NDirections (j);
+                       ++dir)
+                    {
+                      for (int ell = j - 1; ell <= gppword - 2; ell++)
+                        {
+                          // TB_inner(gamma, ord, trefftzbasis, coeff, b, D+1,
+                          // ell, basistype);
+                          Vec<D + 1, int> get_coeff;
+                          get_coeff[D] = 0;
+                          get_coeff[0] = ell + 2;
+                          trefftzbasis (basisn, IndexMap2 (get_coeff, gppword))
+                              = 0;
+                          get_coeff[D] = 1;
+                          get_coeff[0] = ell + 1;
+                          trefftzbasis (basisn, IndexMap2 (get_coeff, gppword))
+                              = 0;
+                          for (int t = 0; t <= ell; t++)
+                            {
+                              int x = ell - t;
+                              get_coeff[D] = t + 2;
+                              get_coeff[0] = x;
+
+                              // cout << "setting " << get_coeff << " mapped to
+                              // " << IndexMap2(get_coeff, gppword) << endl;
+                              Vec<D + 1, int> get_coeff2;
+                              get_coeff2[D] = t;
+                              get_coeff2[0] = x + 2;
+                              trefftzbasis (basisn,
+                                            IndexMap2 (get_coeff, gppword))
+                                  = (x + 2) * (x + 1)
+                                        / ((t + 2) * (t + 1) * gamma[0])
+                                        * trefftzbasis (
+                                            basisn,
+                                            IndexMap2 (get_coeff2, gppword))
+                                    - (x <= j - 2) * BinCoeff (j, t)
+                                          * gamma[x + t + 2 - j]
+                                          * pow (gamma[0], 0.5 * (j - t - 4))
+                                          * TrefftzGppwFE<D>::GetDirection (
+                                              j, dir);
+                              for (int betax = max (0, j - t - 1); betax < x;
+                                   betax++)
+                                {
+                                  get_coeff2[D] = t + 2;
+                                  get_coeff2[0] = betax;
+                                  trefftzbasis (basisn,
+                                                IndexMap2 (get_coeff, gppword))
+                                      -= gamma[x - betax]
+                                         * trefftzbasis (
+                                             basisn,
+                                             IndexMap2 (get_coeff2, gppword))
+                                         / gamma[0];
+                                }
+                            }
+                        }
+                      basisn++;
+                    }
+                }
+              cout << trefftzbasis << endl;
+              cout << "size " << trefftzbasis.Height () << " x "
+                   << trefftzbasis.Width () << endl;
+
+              MatToCSR (trefftzbasis, gtbstore[ord]);
+            }
+        }
+
+      if (gtbstore[ord][0].Size () == 0)
+        // if ( gtbstore.Size() < ord)
+        {
+          stringstream str;
+          str << "failed to generate trefftz basis of order " << ord << endl;
+          throw Exception (str.str ());
+        }
+    }
+
+    const CSR *tb = &gtbstore[ord];
     return tb;
   }
 
   template <int D>
-  void TrefftzGppwBasis<D>::CreateTB (int ord, int gppword, Vector<> gamma,
-                                      int basistype)
-  {
-    cout << "creating tp store for order " << ord << endl;
-
-    // if (tbstore.Size() < ord)
-    //{
-    int oldsize = tbstore.Size ();
-    tbstore.SetSize (ord + 1);
-    for (int i = oldsize; i <= ord; i++)
-      tbstore[i] = CSR ();
-
-    // if ( tbstore[ord][0].Size() == 0)
-    //{
-    const int nbasis = 2 * ord + 1;
-    const int npoly = (gppword + 1) * (gppword + 2) / 2 - 1;
-    Matrix<> trefftzbasis (nbasis, npoly);
-    trefftzbasis = 0;
-    Vec<D + 1, int> coeff = 0;
-    int count = 0;
-    int basisn = 0;
-    for (int j = 0; j <= ord; ++j)
-      for (int dir = 0; dir < TrefftzGppwFE<D>::NDirections (j); ++dir)
-        for (int ell = (basisn++) - 1; ell < gppword; ell++)
-          {
-            // TB_inner(gamma, ord, trefftzbasis, coeff, b, D+1, ell,
-            // basistype);
-            Vec<D + 1, int> get_coeff;
-            get_coeff[D] = 0;
-            get_coeff[0] = ell + 2;
-            trefftzbasis (basisn, IndexMap2 (get_coeff, gppword - 1)) = 0;
-            get_coeff[D] = 1;
-            get_coeff[0] = ell + 1;
-            trefftzbasis (basisn, IndexMap2 (get_coeff, gppword - 1)) = 0;
-            for (int t = 0; t < ell; t++)
-              {
-                int x = ell - t;
-                get_coeff[D] = t + 2;
-                get_coeff[0] = x;
-                Vec<D + 1, int> get_coeff2;
-                get_coeff2[D] = t;
-                get_coeff2[0] = x + 2;
-                trefftzbasis (basisn, IndexMap2 (get_coeff, gppword - 1))
-                    = (x + 2) * (x + 1) / (t + 2) * (t + 2)
-                          * trefftzbasis (basisn,
-                                          IndexMap2 (get_coeff2, gppword - 1))
-                      - (x <= basisn - 2) * BinCoeff (basisn, t);
-              }
-          }
-    MatToCSR (trefftzbasis, tbstore[ord]);
-    //}
-    //}
-
-    // if ( tbstore[ord][0].Size() == 0)
-    if (tbstore.Size () < ord)
-      {
-        stringstream str;
-        str << "failed to generate trefftz basis of order " << ord << endl;
-        throw Exception (str.str ());
-      }
-  }
-
-  template <int D>
-  void TrefftzGppwBasis<D>::TB_inner (Vector<> gamma, int ord,
+  void TrefftzGppwBasis<D>::TB_inner (const Array<double> &gamma, int ord,
                                       Matrix<> &trefftzbasis,
                                       Vec<D + 1, int> coeffnum, int basis,
                                       int dim, int &tracker, int basistype)
@@ -238,12 +434,11 @@ namespace ngfem
   {
     int sum = 0;
     int temp_size = 0;
-    for (int d = 0; d < D; d++)
+    for (int d = 0; d < D + 1; d++)
       {
         for (int p = 0; p < index (d); p++)
           {
-            sum += BinCoeff (D - 1 - d + ord - p - temp_size,
-                             ord - p - temp_size);
+            sum += BinCoeff (D - d + ord - p - temp_size, ord - p - temp_size);
           }
         temp_size += index (d);
       }
