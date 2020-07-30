69e89631be06267bf47fa5d3f7c40d7c0d0da64a
Paul <stocker.paul@hotmail.com>
2020-07-30 16:54:38 +0200

    new adiam with var wavespeed in one element

--- trefftz/trefftzfespace.hpp
+++ trefftz/trefftzfespace.hpp
@@ -67,6 +67,9 @@ namespace ngcomp
         double Adiam(ElementId ei, double c) const;
 
         template<int D>
+        double Adiam(ElementId ei, shared_ptr<CoefficientFunction> c) const;
+
+        template<int D>
         Vec<D+1> ElCenter(ElementId ei) const;
     };
 }
