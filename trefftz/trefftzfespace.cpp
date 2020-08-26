5a7064fcaf5911c3ac39c6a1238dfe9428a4c95a
Paul <stocker.paul@hotmail.com>
2020-08-26 11:50:44 +0200

    fix getfespace return to be more standard

--- trefftz/trefftzfespace.cpp
+++ trefftz/trefftzfespace.cpp
@@ -196,7 +196,6 @@ namespace ngcomp
             }
             break;
           }
-        return *(new (alloc) TrefftzWaveFE<1> ());
       }
     else
       {
@@ -209,7 +208,7 @@ namespace ngcomp
           }
         catch (Exception e)
           {
-            throw Exception ("illegal element type in L2::GetSurfaceFE");
+            throw Exception ("illegal element type in Trefftz::GetSurfaceFE");
           }
       }
   }
