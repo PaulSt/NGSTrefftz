780928e2c8d25550eb5161d0763267bdb18ee9f3
Paul <stocker.paul@hotmail.com>
2020-11-09 18:07:12 +0100

    add qtrefftz test

--- test.py
+++ test.py
@@ -6,14 +6,22 @@ from ngsolve import *
 import time
 
 
-def TestSolution2D(fes,c,timeoffset=0):
+def TestSolution(c=1,timeoffset=0):
     k = 3
     truesol = sin( k*(c*y + x) )
-    v0 = c*k*cos(k*(c*y+x))
     sig0 = -k*cos(k*(c*y+x))
+    v0 = c*k*cos(k*(c*y+x))
     gD = v0
-    U0 = GridFunction(fes)
-    U0.Set(truesol)
+    U0 = truesol
+    return [truesol,U0,sig0,v0,gD]
+
+def TestSolutionSmoothWavespeed(timeoffset=0):
+    ca=2.5
+    truesol = (x+1)**ca * exp(-sqrt(ca*(ca-1))*y)
+    sig0 = -ca*(x+1)**(ca-1) * exp(-sqrt(ca*(ca-1))*y)
+    v0 = -sqrt(ca*(ca-1))*(x+1)**ca * exp(-sqrt(ca*(ca-1))*y)
+    gD = v0
+    U0 = truesol
     return [truesol,U0,sig0,v0,gD]
 
 
@@ -26,7 +34,7 @@ def PostProcess(fes, truesol, sol):
     return [L2error,sH1error]
 
 
-def Cartsolve2D(fes,c,fullsys=False):
+def Cartsolve2D(fes,c,inputsol,fullsys=False):
     """
     We can solve on a simple rectangle grid
     >>> N = 4
@@ -36,15 +44,21 @@ def Cartsolve2D(fes,c,fullsys=False):
 
     using Trefftz basis
     >>> fes = FESpace("trefftzfespace", mesh, order = order, wavespeed = c, dgjumps=True, basistype=0)
-    >>> Cartsolve2D(fes,c) # doctest:+ELLIPSIS
+    >>> Cartsolve2D(fes,c,TestSolution(c)) # doctest:+ELLIPSIS
     [17.0, ..., ...e-09, ...e-08]
 
     or normal L2 basis, requiring the full system
     >>> fes = L2(mesh, order=order, dgjumps=True)
-    >>> Cartsolve2D(fes,c,True) # doctest:+ELLIPSIS
+    >>> Cartsolve2D(fes,c,TestSolution(c),True) # doctest:+ELLIPSIS
     [81.0, ..., ...e-12, ...e-10]
+
+    or quasi-Trefftz basis for smooth wavespeeds, also requiring the full system
+    >>> c = CoefficientFunction((x+1))
+    >>> fes = trefftzfespace(mesh, order=order, dgjumps=True, basistype=0, useshift=True, useqt=True)
+    >>> fes.SetWavespeed(c)
+    >>> Cartsolve2D(fes,c,TestSolutionSmoothWavespeed(),True) # doctest:+ELLIPSIS
+    [17.0, ..., ...e-11, ...e-11]
     """
-    inputsol = TestSolution2D(fes,c)
     [truesol,U0,sig0,v0,gD] = inputsol
 
     start = time.time()
