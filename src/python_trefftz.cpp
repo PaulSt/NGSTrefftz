f765910c2dce88b063ad5567166e2631b7b43aed
Paul <stocker.paul@hotmail.com>
2021-11-14 01:14:49 +0100

    mv airy to own file

--- src/python_trefftz.cpp
+++ src/python_trefftz.cpp
@@ -9,56 +9,13 @@
 //#include <tents.hpp>
 #include "meshtentslab.hpp"
 #include "twavetents.hpp"
-//only for airy
-#include "python_fem.hpp"
-#include <boost/math/special_functions/airy.hpp>
+#include "svdtrefftz.cpp"
 
-namespace ngcomp
-{
-    // Airy function used to test quasi-Trefftz methods
-    struct GenericAiry {
-        double operator() (double x) const { return boost::math::airy_ai(x); }
-        SIMD<double> operator() (SIMD<double> x) const
-        {
-            return SIMD<double>([&](int i)->double
-                    {
-                    return boost::math::airy_ai(x[i]);
-                    });
-        }
-        template <typename T> T operator() (T x) const
-        { throw Exception (string("airy not available for type ")+typeid(T).name());  }
-        template<typename T>
-        AutoDiff<1,T> operator() (AutoDiff<1,T> x) const { throw Exception ("airy(..) is not complex differentiable"); }
-        template<typename T>
-        AutoDiffDiff<1,T> operator() (AutoDiffDiff<1,T> x) const { throw Exception ("airy(..) is not complex differentiable"); }
-        static string Name() { return "airy"; }
-        void DoArchive(Archive& ar) {}
-    };
-
-    struct GenericAiryP {
-        double operator() (double x) const { return boost::math::airy_ai_prime(x); }
-        SIMD<double> operator() (SIMD<double> x) const
-        {
-            return SIMD<double>([&](int i)->double
-                    {
-                    return boost::math::airy_ai_prime(x[i]);
-                    });
-        }
-        template <typename T> T operator() (T x) const
-        { throw Exception (string("airy prime not available for type ")+typeid(T).name());  }
-        template<typename T>
-        AutoDiff<1,T> operator() (AutoDiff<1,T> x) const { throw Exception ("airyp(..) is not complex differentiable"); }
-        template<typename T>
-        AutoDiffDiff<1,T> operator() (AutoDiffDiff<1,T> x) const { throw Exception ("airyp(..) is not complex differentiable"); }
-        static string Name() { return "airyp"; }
-        void DoArchive(Archive& ar) {}
-    };
-}
+#include "airy.cpp"
 
 
 PYBIND11_MODULE(_tngs,m) {
     py::module::import("ngsolve");
-    //py::module::import("ngstents");
     m.attr("__name__") = "tngs";
     m.attr("__package__") = "tngs";
 
@@ -67,6 +24,7 @@ PYBIND11_MODULE(_tngs,m) {
     ExportSpecialCoefficientFunction(m);
     ExportTWaveTents(m);
     ExportMeshTentSlab(m);
+
     ExportStdMathFunction<GenericAiry>(m, "airy", "airy function");
     ExportStdMathFunction<GenericAiryP>(m, "airyp", "airyp function");
 }
