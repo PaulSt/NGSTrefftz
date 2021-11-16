5ed605ce439bf7192bdfbd83119169a24eca5bbb
Paul <stocker.paul@hotmail.com>
2021-11-16 22:42:35 +0100

    trying to make svdtrefftz run with taskmngr

--- src/svdtrefftz.cpp
+++ src/svdtrefftz.cpp
@@ -31,6 +31,8 @@ namespace ngcomp
     shared_ptr<BaseMatrix> SVDTrefftz (shared_ptr<SumOfIntegrals> bf,
                                        shared_ptr<FESpace> fes, double eps)
     {
+        static Timer svdtt("svdtrefftz");
+        svdtt.Start();
         LocalHeap lh(1000 * 1000 * 1000);
 
         auto ma = fes->GetMeshAccess();
@@ -43,64 +45,72 @@ namespace ngcomp
             bfis[dx.vb] += make_shared<SymbolicBilinearFormIntegrator> (icf->cf, dx.vb, dx.element_vb);
         }
 
-        Array<Matrix<double> > elmats;
-        Array<Array<int> > newdofs;
-        int newdofscounter = 0;
-        int max_elsperrow = 0;
-        int Pwidth = 0;
+        Array<double> allvalues(fes->GetNDof()*fes->GetNDof());
+        Array<Matrix<double> > Pmats(ma->GetNE());
+        Array<int> nzs(ma->GetNE());
+
 
         ma->IterateElements(VOL,lh,[&](auto ei, LocalHeap & mlh)
+        //for (auto ei : ma->Elements())
         {
-            HeapReset hr(lh);
+            HeapReset hr(mlh);
             Array<DofId> dofs;
             fes->GetDofNrs(ei, dofs);
-            auto & trafo = ma->GetTrafo(ei, lh);
-            auto & fel = fes->GetFE(ei, lh);
+            auto & trafo = ma->GetTrafo(ei, mlh);
+            auto & fel = fes->GetFE(ei, mlh);
 
-            FlatMatrix<> elmat(dofs.Size(), lh);
-            FlatMatrix<> elmati(dofs.Size(), lh);
+            FlatMatrix<> elmat(dofs.Size(), mlh);
+            FlatMatrix<> elmati(dofs.Size(), mlh);
             elmat = 0.0;
             for (auto & bfi : bfis[VOL])
             {
-                bfi -> CalcElementMatrix(fel, trafo, elmati, lh);
+                bfi -> CalcElementMatrix(fel, trafo, elmati, mlh);
                 elmat += elmati;
             }
             Matrix<double,ColMajor> U(dofs.Size(),dofs.Size()), V(dofs.Size(),dofs.Size());
             //CalcSVD(elmat,U,V);
             LapackSVD(elmat,U,V);
-            Array<int> newdof;
-            for(auto sv : elmat.Diag()) if(sv<eps) newdof.Append(newdofscounter++);
-            Pwidth += newdof.Size();
-            newdofs.Append(newdof);
-            elmats.Append(U.Cols(dofs.Size()-newdof.Size(),dofs.Size()));
-        });
-
-        int nnewndof = newdofs.Last().Last();
-
+            int nz = 0;
+            for(auto sv : elmat.Diag()) if(sv<eps) nz++;
+            nzs[ei.Nr()]=nz;
+            //Pwidth += nz;
+            //Pmats[ei.Nr()].AssignMemory (dofs.Size(),dofs.Size(), allvalues.Addr(dofs.Size()*dofs.Size()*ei.Nr()));
+            Pmats[ei.Nr()] = (U.Cols(dofs.Size()-nz,dofs.Size()));
+        }
+        );
 
         TableCreator<int> creator(ma->GetNE(VOL));
         TableCreator<int> creator2(ma->GetNE(VOL));
         for ( ; !creator.Done(); creator++, creator2++)
             ma->IterateElements(VOL,lh,[&](auto ei, LocalHeap & mlh)
+            //for (auto ei : ma->Elements())
                                 {
                                     Array<DofId> dnums;
                                     fes->GetDofNrs (ei, dnums);
                                     for (DofId d : dnums)
-                                        if (IsRegularDof(d)) creator.Add (ei.Nr(), d);
-                                    for (int d : newdofs[ei.Nr()])
-                                        if (IsRegularDof(d)) creator2.Add (ei.Nr(), d);
-                                });
+                                        //if (IsRegularDof(d))
+                                        creator.Add (ei.Nr(), d);
+                                    int prevdofs = 0;
+                                    for (int i=0;i<ei.Nr();i++) prevdofs += nzs[ei.Nr()];//Pmats[ei.Nr()].Width();
+                                    for (int d=0;d<nzs[ei.Nr()];d++)
+                                        creator2.Add (ei.Nr(), d+prevdofs);
+                                }
+        );
         auto table = creator.MoveTable();
         auto table2 = creator2.MoveTable();
 
+        int Pwidth = 0;
+        for(auto ei : ma->Elements()) Pwidth += nzs[ei.Nr()];
         SparseMatrix<double> P(fes->GetNDof(), Pwidth, table,table2,false);
 
-        for(auto ei : ma->Elements())
+        ma->IterateElements(VOL,lh,[&](auto ei, LocalHeap & mlh)
+        //for(auto ei : ma->Elements())
         {
-            Array<DofId> dofs;
-            fes->GetDofNrs(ei, dofs);
-            P.AddElementMatrix(dofs,newdofs[ei.Nr()], elmats[ei.Nr()]);
-        }
+            //Array<DofId> dofs;
+            //fes->GetDofNrs(ei, dofs);
+            P.AddElementMatrix(table[ei.Nr()],table2[ei.Nr()], Pmats[ei.Nr()]);
+        });
+        svdtt.Stop();
         return make_shared<SparseMatrix<double>>(P);
     }
 }
