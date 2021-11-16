6e8a0cca6e9efc1fa0af8189cdc59c601330da3a
Paul <stocker.paul@hotmail.com>
2021-11-16 16:20:12 +0100

    cleanup svdtrefftz

--- svdtrefftz.py
+++ svdtrefftz.py
@@ -6,39 +6,43 @@ import scipy as sp
 import numpy as np
 import matplotlib.pylab as plt
 
-printer=0
 def trunc(values, decs=0):
     return np.trunc(values*10**decs)/(10**decs)
+def spspy(sparsemat):
+    rows,cols,vals = sparsemat.COO()
+    A = sp.sparse.csr_matrix((vals,(rows,cols)))
+    # A = A.todense()
+    plt.spy(A); plt.plot(); plt.show()
 
 def LocalP(A,eps=10**-9):
-    global printer 
+    global printer
     U,s,V = sp.linalg.svd(A)
     nz = (s<eps).sum()
     nnz = (s>=eps).sum()
-    # print(s)
-    # print(nnz,nz, " vs ", 2*3+1)
     S=np.diag(s)
-    # V = V[nnz:,nnz:]
-    # U = U[:,nnz:]
-    # P=U@S@V
     P = U[:,nnz:]
-    if(printer==0):
-        # print(A)
-        # print(trunc(U@np.diag(s)@V,decs=0))
-        # print(s<eps)
-        # print(s)
-        # print((s<eps)[nnz:])
-        print("U \n",trunc(U,2))
-        print("V \n",trunc(V,2))
-        # print(trunc(U@S@V,2))
-        # print("P",trunc(P))
-        # print(P.shape)
-        printer=1
+    return P
+
+def PySVDTrefftz(op,fes,eps=10**-10):
+    opbf = BilinearForm(fes)
+    opbf += op
+    opbf.Assemble()
+    rows,cols,vals = opbf.mat.COO()
+    A = sp.sparse.csr_matrix((vals,(rows,cols)))
+    A = A.todense()
+    localndof = int(fes.ndof/mesh.ne)
+    trefftzndof = 2*order+1
+    P = np.zeros([A.shape[1],(trefftzndof)*mesh.ne])
+    for i in range(mesh.ne):
+        # U,s,V = sp.linalg.svd(A[i*localndof:(i+1)*localndof,i*localndof:(i+1)*localndof])
+        elmat = A[i*localndof:(i+1)*localndof,i*localndof:(i+1)*localndof]
+        LP = LocalP(elmat)
+        P[i*localndof:(i+1)*localndof,i*trefftzndof:(i+1)*trefftzndof] = LP
     return P
 
 
-order = 2
-mesh = Mesh(unit_square.GenerateMesh(maxh=0.5))
+order = 7
+mesh = Mesh(unit_square.GenerateMesh(maxh=0.3))
 fes = L2(mesh, order=order,  dgjumps=True)#,all_dofs_together=True)
 n = specialcf.normal(mesh.dim)
 exact = exp(x)*sin(y)
@@ -67,63 +71,42 @@ a += grad(u)*grad(v) * dx \
 a.Assemble()
 f.Assemble()
 
-opbf = BilinearForm(fes)
-# opbf += grad(u)*grad(v) * dx
-# opbf += InnerProduct(u.Operator("hesse"),v.Operator("hesse"))*dx
 uh = u.Operator("hesse")
 vh = v.Operator("hesse")
 op = (uh[0,0]+uh[1,1])*(vh[0,0]+vh[1,1])*dx
-opbf += op
-opbf.Assemble()
-rows,cols,vals = opbf.mat.COO()
-A = sp.sparse.csr_matrix((vals,(rows,cols)))
-# plt.spy(A); plt.plot(); plt.show()
-A = A.todense()
+# op = grad(u)*grad(v) * dx
+# op = InnerProduct(u.Operator("hesse"),v.Operator("hesse"))*dx
 
-localndof = int(fes.ndof/mesh.ne)
-trefftzndof = 2*order+1
-P = np.zeros([A.shape[1],(trefftzndof)*mesh.ne])
-for i in range(mesh.ne):
-    # U,s,V = sp.linalg.svd(A[i*localndof:(i+1)*localndof,i*localndof:(i+1)*localndof])
-    elmat = A[i*localndof:(i+1)*localndof,i*localndof:(i+1)*localndof]
-    # print(i," elmat=",trunc(elmat))
-    LP = LocalP(elmat)
-    P[i*localndof:(i+1)*localndof,i*trefftzndof:(i+1)*trefftzndof] = LP
-    # input(i)
 
-rows,cols,vals = a.mat.COO()
-A = sp.sparse.csr_matrix((vals,(rows,cols)))
-A = A.todense()
-AA=P.transpose()@A@P
-
-start = time.time()
-tsol = np.linalg.solve(AA,P.transpose()@f.vec.FV())
-tpgfu = GridFunction(fes)
-tpgfu.vec.data = P@tsol
-print("trefftz time: ", time.time()-start)
 
 start = time.time()
 gfu = GridFunction(fes)
-gfu.vec.data = np.linalg.solve(A,f.vec.FV())#a.mat.Inverse() * f.vec
+# gfu.vec.data = np.linalg.solve(A,f.vec.FV())
+gfu.vec.data = a.mat.Inverse() * f.vec
 print("time: ", time.time()-start)
-
-
 print ("L2-error:", sqrt(Integrate((gfu-exact)**2, mesh)))
-print ("vs Trefftz L2-error:", sqrt(Integrate((tpgfu-exact)**2, mesh)))
 
-plt.spy(P); plt.plot(); plt.show()
-PP = SVDTrefftz(op,fes)
-# rows,cols,vals = PP.COO()
-# PP = sp.sparse.csr_matrix((vals,(rows,cols)))
-# plt.spy(PP); plt.plot(); plt.show()
+a.DeleteMatrix()
+a.Assemble()
+start = time.time()
+PP = SVDTrefftz(op,fes,10**-9)
 PPT = PP.CreateTranspose()
 na = PPT@a.mat@PP
-# tu = PP.CreateRowVector()
 tu = na.Inverse()*(PPT*f.vec)
 tpgfu = GridFunction(fes)
 tpgfu.vec.data = PP*tu
+print("trefftz time: ", time.time()-start)
 print ("vs Trefftz L2-error:", sqrt(Integrate((tpgfu-exact)**2, mesh)))
 
-# rows,cols,vals = na.COO()
-# naa = sp.sparse.csr_matrix((vals,(rows,cols)))
-# plt.spy(naa); plt.plot(); plt.show()
+
+P = PySVDTrefftz(op,fes)
+rows,cols,vals = a.mat.COO()
+A = sp.sparse.csr_matrix((vals,(rows,cols)))
+A = A.todense()
+TA = P.transpose()@A@P
+
+tsol = np.linalg.solve(TA,P.transpose()@f.vec.FV())
+tpgfu = GridFunction(fes)
+tpgfu.vec.data = P@tsol
+print ("vs PythonTrefftz L2-error:", sqrt(Integrate((tpgfu-exact)**2, mesh)))
+print(P.shape,"vs",PP.shape)
