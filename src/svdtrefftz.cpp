6c8dad8e157bdac537abcb5b3275222b50ebc42a
Paul <stocker.paul@hotmail.com>
2021-11-16 15:23:59 +0100

    svdtrefftz working

--- src/svdtrefftz.cpp
+++ src/svdtrefftz.cpp
@@ -4,27 +4,27 @@
 
 namespace ngcomp
 {
-
-  class ConstCOO
+  void LapackSVD (SliceMatrix<> A, SliceMatrix<double, ColMajor> U,
+                  SliceMatrix<double, ColMajor> V)
   {
-  public:
-    ConstCOO () { ; }
-    Array<int> rowind;
-    Array<int> colind;
-    Array<double> data;
+    static Timer t ("LapackSVD");
+    RegionTimer reg (t);
+    ngbla::integer m = A.Width (), n = A.Height ();
+    // Matrix<> U(m), V(n);
+    Vector<> S (min (n, m));
+    Array<double> work (n * m + 100);
+    ngbla::integer info;
+    char jobu = 'A', jobv = 'A';
+    ngbla::integer lda = A.Dist (), ldu = U.Dist (), ldv = V.Dist ();
+    ngbla::integer lwork = work.Size ();
 
-    void AppendElmat (const Array<int> &dofs1, const Array<int> &dofs2,
-                      FlatMatrix<> elmat)
-    {
-      for (int i = 0; i < dofs1.Size (); i++)
-        for (int j = 0; j < dofs2.Size (); j++)
-          {
-            rowind.Append (dofs1[i]);
-            colind.Append (dofs2[j]);
-            data.Append (elmat (i, j));
-          }
-    };
-  };
+    dgesvd_ (&jobu, &jobv, &m, &n, A.Data (), &lda, S.Data (), U.Data (), &ldu,
+             V.Data (), &ldv, work.Data (), &lwork, &info);
+    // cout << "info = " << info << endl;
+    // if (n <= 100)
+    // cout << "S = " << S << endl;
+    A.Diag (0) = S;
+  }
 
   shared_ptr<BaseMatrix> SVDTrefftz (shared_ptr<SumOfIntegrals> bf,
                                      shared_ptr<FESpace> fes, double eps)
@@ -48,97 +48,107 @@ namespace ngcomp
       }
 
     // Array<FlatMatrix<double> > elmats(ma->GetNE());
-    // Array<Matrix<double> > elmats;
+    Array<Matrix<double>> elmats;
     Array<Array<int>> newdofs;
     int newdofscounter = 0;
     // Array<int> widths;
     // size_t Tndofs = 0;
     int max_elsperrow = 0;
     int Pwidth = 0;
-    ConstCOO elmats;
 
-    ma->IterateElements (VOL, lh,
-                         [&] (auto ei, LocalHeap &mlh)
-                         // for(auto ei : ma->Elements())
-                         {
-                           HeapReset hr (lh);
-                           // ElementId ei(VOL, el);
-                           // cout << "ei = " << ei << endl;
-                           Array<DofId> dofs;
-                           fes->GetDofNrs (ei, dofs);
-                           // cout << "dofs = " << dofs << endl;
-                           auto &trafo = ma->GetTrafo (ei, lh);
-                           auto &fel = fes->GetFE (ei, lh);
+    ma->IterateElements (
+        VOL, lh,
+        [&] (auto ei, LocalHeap &mlh)
+        // for(auto ei : ma->Elements())
+        {
+          HeapReset hr (lh);
+          // ElementId ei(VOL, el);
+          // cout << "ei = " << ei << endl;
+          Array<DofId> dofs;
+          fes->GetDofNrs (ei, dofs);
+          // cout << "dofs = " << dofs << endl;
+          auto &trafo = ma->GetTrafo (ei, lh);
+          auto &fel = fes->GetFE (ei, lh);
 
-                           FlatMatrix<> elmat (dofs.Size (), lh);
-                           FlatMatrix<> elmati (dofs.Size (), lh);
-                           elmat = 0.0;
-                           for (auto &bfi : bfis[VOL])
-                             {
-                               bfi->CalcElementMatrix (fel, trafo, elmati, lh);
-                               elmat += elmati;
-                               // bfi -> CalcElementMatrixAdd(fel, trafo,
-                               // elmat, lh);
-                             }
-                           // cout << ei << " elmat = " << elmat << endl;
-                           SliceMatrix<> A (elmat);
-                           Matrix<double, ColMajor> U (dofs.Size (),
-                                                       dofs.Size ()),
-                               V (dofs.Size (), dofs.Size ());
-                           CalcSVD (elmat, U, V);
-                           // int nnz=0;
-                           Array<int> newdof;
-                           for (auto sv : elmat.Diag ())
-                             if (sv < eps)
-                               newdof.Append (newdofscounter++);
-                           Pwidth += newdof.Size ();
-                           // newdofs.Append(newdof);
-                           // max_elsperrow =
-                           // max_elsperrow<newdof.Size()?newdof.Size():max_elsperrow;
-                           // cout << newdof.Size();
-                           // cout << nnz <<  "SVD" << elmat.Diag() << endl <<
-                           // U << endl << V << endl; FlatMatrix<> P =
-                           // U.Cols(nnz,dofs.Size()); cout <<"P"<<endl<< P <<
-                           // endl;
-                           // elmats.Append(U.Cols(dofs.Size()-newdof.Size(),dofs.Size()));
-                           // widths.Append(dofs.Size()-nnz);
-                           // Tndofs += dofs.Size()-nnz;
-                           //(elmats[ei.Nr()]
-                           elmats.AppendElmat (dofs, newdof, elmat);
-                         });
+          FlatMatrix<> elmat (dofs.Size (), lh);
+          FlatMatrix<> elmati (dofs.Size (), lh);
+          elmat = 0.0;
+          for (auto &bfi : bfis[VOL])
+            {
+              bfi->CalcElementMatrix (fel, trafo, elmati, lh);
+              elmat += elmati;
+              // bfi -> CalcElementMatrixAdd(fel, trafo, elmat, lh);
+            }
+          // cout << ei << " elmat = " << elmat << endl;
+          // cout << elmat << endl;
+          Matrix<double, ColMajor> U (dofs.Size (), dofs.Size ()),
+              V (dofs.Size (), dofs.Size ());
+          // CalcSVD(elmat,U,V);
+          LapackSVD (elmat, U, V);
+          // int nnz=0;
+          Array<int> newdof;
+          for (auto sv : elmat.Diag ())
+            if (sv < eps)
+              newdof.Append (newdofscounter++);
+          Pwidth += newdof.Size ();
+          newdofs.Append (newdof);
+          // max_elsperrow =
+          // max_elsperrow<newdof.Size()?newdof.Size():max_elsperrow; cout <<
+          // newdof.Size(); cout << nnz <<  "SVD" << elmat.Diag() << endl << U
+          // << endl << V << endl; FlatMatrix<> P = U.Cols(nnz,dofs.Size());
+          // cout <<"P"<<endl<< P << endl;
+          // cout << "U"<< endl<<U <<endl;
+          // cout << "V**T"<< endl<<V <<endl;
+          // cout << elmat << endl;
+          // cout << endl << "U";
+          // for(int i=0;i<dofs.Size();i++)
+          //{
+          // cout << endl;
+          // for(int j=0;j<dofs.Size();j++)
+          //{
+          // cout << U(i,j) << " ";
+          // }
+          //}
+          // cout << endl;
+          elmats.Append (U.Cols (dofs.Size () - newdof.Size (), dofs.Size ()));
+          // widths.Append(dofs.Size()-nnz);
+          // Tndofs += dofs.Size()-nnz;
+          //(elmats[ei.Nr()]
+        });
 
-    // int nnewndof = newdofs.Last().Last();
-    // cout << endl<<nnewndof<<endl;
-    // ElementByElementMatrix<double> P(fes->GetNDof(), 29, ma->GetNE(),
-    // false); cout << fes->GetNDof() << ","<<max_elsperrow << endl;
+    int nnewndof = newdofs.Last ().Last ();
+
+    TableCreator<int> creator (ma->GetNE (VOL));
+    for (; !creator.Done (); creator++)
+      ma->IterateElements (VOL, lh, [&] (auto ei, LocalHeap &mlh) {
+        Array<DofId> dnums;
+        fes->GetDofNrs (ei, dnums);
+        for (DofId d : dnums)
+          if (IsRegularDof (d))
+            creator.Add (ei.Nr (), d);
+      });
+    auto table = creator.MoveTable ();
+    cout << table << endl;
+    TableCreator<int> creator2 (ma->GetNE (VOL));
+    for (; !creator2.Done (); creator2++)
+      ma->IterateElements (VOL, lh, [&] (auto ei, LocalHeap &mlh) {
+        for (int d : newdofs[ei.Nr ()])
+          if (IsRegularDof (d))
+            creator2.Add (ei.Nr (), d);
+      });
+    auto table2 = creator2.MoveTable ();
+    cout << table2 << endl;
+
     // SparseMatrix<double> P(fes->GetNDof(), max_elsperrow);
-    // cout << "fisti"<<P.First(0)<<endl;
-    // cout << "fisti"<<P.GetFirstArray()<<endl;
-    // cout << "colin"<<P.GetColIndices()<<endl;
-    // cout << P.GetRowIndices(0) << endl;
-    // for(auto ei : ma->Elements())
-    //{
-    // Array<DofId> dofs;
-    // fes->GetDofNrs(ei, dofs);
-    ////cout << dofs << " and " << newdofs[ei.Nr()] << endl;
-    ////P.AddElementMatrix(ei.Nr(),dofs,newdofs[ei.Nr()], elmats[ei.Nr()]);
-    // for(int i : dofs)
-    // for(int j : newdofs[ei.Nr()])
-    //{
-    // P.CreatePosition(i,j);
-    // cout << i<<","<<j<< endl;
-    //}
-    // P.AddElementMatrix(dofs,newdofs[ei.Nr()], elmats[ei.Nr()]);
-    ////cout << dofs.Range(0,widths[ei.Nr()]) << endl;
-    ////cout << newdofs[ei.Nr()];
-    //}
-    // return make_shared<ElementByElementMatrix<double>>(P);
+    SparseMatrix<double> P (fes->GetNDof (), Pwidth, table, table2, false);
 
-    shared_ptr<SparseMatrixTM<double>> P
-        = SparseMatrixTM<double>::CreateFromCOO (elmats.rowind, elmats.colind,
-                                                 elmats.data, fes->GetNDof (),
-                                                 Pwidth);
-    return P;
+    for (auto ei : ma->Elements ())
+      {
+        Array<DofId> dofs;
+        fes->GetDofNrs (ei, dofs);
+        P.AddElementMatrix (dofs, newdofs[ei.Nr ()], elmats[ei.Nr ()]);
+      }
+    return make_shared<SparseMatrix<double>> (P);
   }
 }
 
