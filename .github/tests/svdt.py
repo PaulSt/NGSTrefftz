cdbda292e7d5cc7237ffb7e4e67c93103aacffc3
Paul <stocker.paul@hotmail.com>
2021-11-22 12:13:35 +0100

    add svd test, update tests, remove boost dep

--- .github/tests/svdt.py
+++ .github/tests/svdt.py
@@ -4,19 +4,34 @@ import time
 from netgen.geom2d import unit_square
 import scipy as sp
 import numpy as np
-import matplotlib.pylab as plt
 
-def trunc(values, decs=0):
-    return np.round(values*10**decs)/(10**decs)
-def spspy(sparsemat):
-    rows,cols,vals = sparsemat.COO()
-    A = sp.sparse.csr_matrix((vals,(rows,cols)))
-    # A = A.todense()
-    plt.spy(A); plt.plot(); plt.show()
+# import matplotlib.pylab as plt
+# def trunc(values, decs=0):
+    # return np.round(values*10**decs)/(10**decs)
+# def spspy(sparsemat):
+    # rows,cols,vals = sparsemat.COO()
+    # A = sp.sparse.csr_matrix((vals,(rows,cols)))
+    # # A = A.todense()
+    # plt.spy(A); plt.plot(); plt.show()
 
+order = 5
+exact = exp(x)*sin(y)
+eps = 10**-8
+mesh = Mesh(unit_square.GenerateMesh(maxh=0.3))
+SetNumThreads(3)
 
-
+########################################################################
+# L2
+########################################################################
 def dglap(fes,bndc):
+    """
+    >>> fes = L2(mesh, order=order,  dgjumps=True)#,all_dofs_together=True)
+    >>> a,f = dglap(fes,exact)
+    >>> gfu = GridFunction(fes)
+    >>> gfu.vec.data = a.mat.Inverse() * f.vec
+    >>> sqrt(Integrate((gfu-exact)**2, mesh)) # doctest:+ELLIPSIS
+    4...e-09
+    """
     mesh = fes.mesh
     order = fes.globalorder
     alpha = 4
@@ -44,6 +59,11 @@ def dglap(fes,bndc):
     f.Assemble()
     return a,f
 
+
+########################################################################
+# PySVDTrefftz
+########################################################################
+
 def LocalP(A,eps=10**-9,printP=0):
     U,s,V = np.linalg.svd(A)
     nz = (s<eps).sum()
@@ -57,6 +77,26 @@ def LocalP(A,eps=10**-9,printP=0):
     return P
 
 def PySVDTrefftz(op,fes,eps):
+    """
+    >>> fes = L2(mesh, order=order,  dgjumps=True)#,all_dofs_together=True)
+    >>> u,v = fes.TnT()
+    >>> uh = u.Operator("hesse")
+    >>> vh = v.Operator("hesse")
+    >>> op = (uh[0,0]+uh[1,1])*(vh[0,0]+vh[1,1])*dx
+    >>> P = PySVDTrefftz(op,fes,eps)
+    >>> a,f = dglap(fes,exact)
+    >>> rows,cols,vals = a.mat.COO()
+    >>> A = sp.sparse.csr_matrix((vals,(rows,cols)))
+    >>> A = A.todense()
+    >>> TA = P.transpose()@A@P
+
+    >>> tsol = np.linalg.solve(TA,P.transpose()@f.vec.FV())
+    >>> tpgfu = GridFunction(fes)
+    >>> tpgfu.vec.data = P@tsol
+    >>> sqrt(Integrate((tpgfu-exact)**2, mesh)) # doctest:+ELLIPSIS
+    1...e-08
+    """
+
     opbf = BilinearForm(fes)
     opbf += op
     opbf.Assemble()
@@ -71,33 +111,21 @@ def PySVDTrefftz(op,fes,eps):
         elmat = A[i*localndof:(i+1)*localndof,i*localndof:(i+1)*localndof]
         LP = LocalP(elmat,eps) #,i+1==mesh.ne)
         P[i*localndof:(i+1)*localndof,i*trefftzndof:(i+1)*trefftzndof] = LP
+    # plt.spy(P); plt.plot(); plt.show()
     return P
 
 
-order = 6
-exact = exp(x)*sin(y)
-eps = 10**-7
 
-mesh = Mesh(unit_square.GenerateMesh(maxh=0.1))
-SetNumThreads(3)
-for order in range(3,7):
-    print("ORDER ",order)
 
-    ########################################################################
-    # L2
-    ########################################################################
-    start = time.time()
-    fes = L2(mesh, order=order,  dgjumps=True)#,all_dofs_together=True)
-    a,f = dglap(fes,exact)
-    gfu = GridFunction(fes)
-    # gfu.vec.data = np.linalg.solve(A,f.vec.FV())
-    gfu.vec.data = a.mat.Inverse() * f.vec
-    print("time: ", time.time()-start)
-    print ("L2-error:", sqrt(Integrate((gfu-exact)**2, mesh)))
 
-    ########################################################################
-    # SVDTrefftz
-    ########################################################################
+########################################################################
+# SVDTrefftz
+########################################################################
+def testsvdtrefftz():
+    """
+    >>> testsvdtrefftz() # doctest:+ELLIPSIS
+    1...e-08
+    """
     start = time.time()
     fes = L2(mesh, order=order,  dgjumps=True)#,all_dofs_together=True)
     u,v = fes.TnT()
@@ -115,41 +143,29 @@ for order in range(3,7):
     TU = TA.Inverse()*(PPT*f.vec)
     tpgfu = GridFunction(fes)
     tpgfu.vec.data = PP*TU
-    print ("SVDTrefftz L2-error:", sqrt(Integrate((tpgfu-exact)**2, mesh)))
-    print("trefftz time: ", time.time()-start)
-    for t in Timers():
-        if 'svdt' in t['name']:
-            print(t)
+    return sqrt(Integrate((tpgfu-exact)**2, mesh))
+    # print("trefftz time: ", time.time()-start)
+    # for t in Timers():
+        # if 'svdt' in t['name']:
+            # print(t)
 
 
-    ########################################################################
-    # PySVDTrefftz
-    ########################################################################
-    fes = L2(mesh, order=order,  dgjumps=True)#,all_dofs_together=True)
-    u,v = fes.TnT()
-    uh = u.Operator("hesse")
-    vh = v.Operator("hesse")
-    op = (uh[0,0]+uh[1,1])*(vh[0,0]+vh[1,1])*dx
-    P = PySVDTrefftz(op,fes,eps)
-    # plt.spy(P); plt.plot(); plt.show()
-    a,f = dglap(fes,exact)
-    rows,cols,vals = a.mat.COO()
-    A = sp.sparse.csr_matrix((vals,(rows,cols)))
-    A = A.todense()
-    TA = P.transpose()@A@P
-
-    tsol = np.linalg.solve(TA,P.transpose()@f.vec.FV())
-    tpgfu = GridFunction(fes)
-    tpgfu.vec.data = P@tsol
-    print ("PySVDTrefftz L2-error:", sqrt(Integrate((tpgfu-exact)**2, mesh)))
-    # print(P.shape,"vs",PP.shape)
 
 
-    ########################################################################
-    # Trefftz
-    ########################################################################
+########################################################################
+# Trefftz
+########################################################################
+def testlaptrefftz():
+    """
+    >>> testlaptrefftz() # doctest:+ELLIPSIS
+    1...e-08
+    """
     fes = FESpace("trefftzfespace",mesh,order=order,eq="laplace")
     a,f = dglap(fes,exact)
     gfu = GridFunction(fes)
     gfu.vec.data = a.mat.Inverse() * f.vec
-    print ("Trefftz L2-error:", sqrt(Integrate((gfu-exact)**2, mesh)))
+    return sqrt(Integrate((gfu-exact)**2, mesh))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
--- .github/tests/tents.py
+++ .github/tests/tents.py
@@ -8,149 +8,11 @@ import time
 
 # USE tenthight = wavespeed + 3
 
-def TestSolution2D(fes,c,timeoffset=0):
-    k = 3
-    truesol = sin( k*(c*y + x) )
-    v0 = c*k*cos(k*(c*y+x))
-    sig0 = -k*cos(k*(c*y+x))
-    gD = v0
-    U0 = GridFunction(fes)
-    U0.Set(truesol)
-    return [truesol,U0,sig0,v0,gD]
-
-
-def PostProcess(fes, truesol, sol):
-    mesh = fes.mesh
-    U = GridFunction(fes)
-    U.Set(truesol)
-    L2error = sqrt(Integrate((truesol - sol)*(truesol - sol), mesh))
-    sH1error = sqrt(Integrate((grad(U) - grad(sol))*(grad(U) - grad(sol)), mesh))
-    return [L2error,sH1error]
-
-
-def Cartsolve2D(fes,c,fullsys=False,inputsol=None):
-    """
-    We can solve on a simple rectangle grid
-    >>> N = 4
-    >>> c = 2
-    >>> order = 8
-    >>> mesh = CartSquare(N,c*N)
-
-    using Trefftz basis
-    >>> fes = FESpace("trefftzfespace", mesh, order = order, wavespeed = c, dgjumps=True, basistype=0)
-    >>> Cartsolve2D(fes,c) # doctest:+ELLIPSIS
-    [17.0, ..., ...e-09, ...e-08]
-
-    or normal L2 basis, requiring the full system
-    >>> fes = L2(mesh, order=order, dgjumps=True)
-    >>> Cartsolve2D(fes,c,True) # doctest:+ELLIPSIS
-    [81.0, ..., ...e-12, ...e-10]
-    """
-    if inputsol is None:
-        inputsol = TestSolution2D(fes,c)
-    [truesol,U0,sig0,v0,gD] = inputsol
-
-    start = time.time()
-    [a,f] = DGwaveeqsys(fes,U0,v0,sig0,c,gD,fullsys,False,0.5,0.5,1)
-    # print("DGsys: ", str(time.clock()-start))
-
-    start = time.time()
-    gfu = DGsolve(fes,a,f)
-    cond = 0
-    # print("DGsolve: ", str(time.clock()-start))
-
-    [L2error, sH1error] = PostProcess(fes,truesol,gfu)
-
-    dof=fes.ndof/fes.mesh.ne
-
-    return [dof,cond,L2error,sH1error]
-
-
-
-def TestAiry(order, mesh, t_step,qtrefftz=1):
-    """
-    Solve with quasi-Trefftz basis functions
-    >>> order = 4
-    >>> SetNumThreads(1)
-    >>> t_step = 1
-    >>> for h in [4,8,16,32]:
-    ...        mesh = CartSquare(h,h)
-    ...        TestAiry(order,mesh,t_step) # doctest:+ELLIPSIS
-    5...e-05
-    ...e-06
-    ...e-07
-    ...e-08
-    """
-    c=1
-    bdd = CoefficientFunction((
-        airy(-x-c)*cos(y),
-        -airyp(-x-c)*cos(y),
-        -airy(-x-c)*sin(y)
-        ))
-    wavespeed=CoefficientFunction(1/sqrt(c+x))
-
-    U0=bdd[0]
-    gD=bdd[2]
-    v0=bdd[2]
-    sig0=-bdd[1]
-
-    fes = trefftzfespace(mesh, order=order, dgjumps=True, basistype=0, useshift=True, useqt=True)
-    fes.SetWavespeed(wavespeed)
-    [a,f] = DGwaveeqsys(fes,U0,v0,sig0,wavespeed,gD,True,False,alpha=0.5,beta=0.5,gamma=1,mu=0.5)
-    gfu = DGsolve(fes,a,f)
-    dgerror = DGnormerror(fes,gfu,bdd[1:3],wavespeed,alpha=0.5,beta=0.5)
-
-    return dgerror
-
-
-def TestBessel(order, mesh, t_step):
-    """
-    Solve using tent pitching and quasi-Trefftz basis functions
-    >>> order = 4
-    >>> SetNumThreads(1)
-    >>> t_step = 1
-    >>> for h in [4,8,16,32]:
-    ...        mesh = CartSquare(h,h,xshift=3)
-    ...        TestBessel(order,mesh,t_step) # doctest:+ELLIPSIS
-    9...e-06
-    ...e-07
-    ...e-08
-    ...e-09
-    """
-
-    D = mesh.dim
-    t = CoordCF(D)
-    t_start = 0
-
-    c=0
-    bdd = CoefficientFunction((
-        ((x+c)**(-2)*sin(x+c)-(x+c)**(-1)*cos(x+c)) * cos(y),
-        (-2*(x+c)**(-3)*sin(x+c) + 2*(x+c)**(-2)*cos(x+c) + (x+c)**(-1)*sin(x+c)) * cos(y),
-        -((x+c)**(-2)*sin(x+c)-(x+c)**(-1)*cos(x+c)) * sin(y)
-        ))
-    wavespeed=1/sqrt((x+c)**2-2)
-    BB = (x+c)**2
-
-    U0=bdd[0]
-    gD=bdd[2]
-    v0=bdd[2]
-    sig0=-bdd[1]
-
-    fes = trefftzfespace(mesh, order=order, dgjumps=True, basistype=0, useshift=True,usescale=False, useqt=True)
-    fes.SetWavespeed(wavespeed,BB)
-    [a,f] = DGwaveeqsys(fes,U0,v0,sig0,wavespeed,gD,True,False,alpha=0,beta=0,gamma=1,mu=0,BB=BB)
-    gfu = DGsolve(fes,a,f)
-    dgerror = DGnormerror(fes,gfu,bdd[1:3],wavespeed,alpha=0,beta=0,BB=BB)
-
-    return dgerror
-
-
-
 def SolveWaveTents(initmesh, order, c, t_step):
     """
     Solve using tent pitching
     >>> order = 4
-    >>> SetNumThreads(2)
+    >>> SetNumThreads(4)
     >>> c = 1
     >>> t_step = 2/sqrt(3)
     >>> meshes=[ Mesh(SegMesh(4,0,math.pi)), Mesh(unit_square.GenerateMesh(maxh = 0.4)) , Mesh(unit_cube.GenerateMesh(maxh = 1))]
@@ -250,37 +112,37 @@ def SolveWaveTents(initmesh, order, c, t_step):
 
 
 
-def TestAiryTent(order, initmesh, t_step,qtrefftz=1):
+def TestQTrefftz(order, initmesh, t_step,qtrefftz=1):
     """
     Solve using tent pitching and quasi-Trefftz basis functions
     >>> order = 4
-    >>> SetNumThreads(2)
+    >>> SetNumThreads(4)
     >>> t_step = 1
     >>> for h in [4,8,16,32]:
     ...        initmesh = Mesh(SegMesh(h,0,math.pi))
-    ...        TestAiryTent(order,initmesh,t_step) # doctest:+ELLIPSIS
-    0.01...
-    0.0007...
+    ...        TestQTrefftz(order,initmesh,t_step) # doctest:+ELLIPSIS
+    0.02...
+    0.0011...
     ...e-05
     ...e-06
     >>> initmesh = Mesh(unit_square.GenerateMesh(maxh = 0.5))
     >>> for h in range(4):
-    ...        TestAiryTent(order,initmesh,t_step) # doctest:+ELLIPSIS
+    ...        TestQTrefftz(order,initmesh,t_step) # doctest:+ELLIPSIS
     ...        initmesh.Refine()
     0.004...
-    0.0007...
+    0.0015...
     ...e-05
     ...e-06
 
     Compare to standard Trefftz basis
     >>> initmesh = Mesh(unit_square.GenerateMesh(maxh = 0.5))
     >>> for h in range(4):
-    ...        TestAiryTent(order,initmesh,t_step,None) # doctest:+ELLIPSIS
+    ...        TestQTrefftz(order,initmesh,t_step,None) # doctest:+ELLIPSIS
     ...        initmesh.Refine()
-    0.01...
+    0.12...
+    0.025...
+    0.007...
     0.001...
-    0.0005...
-    0.0001...
     """
 
     # for i in range(0,len(initmesh.GetBoundaries())):
@@ -290,27 +152,30 @@ def TestAiryTent(order, initmesh, t_step,qtrefftz=1):
     t_start = 0
 
     c=1
-    if D is 1:
+    if D==1:
+        ca=2.5
         bdd = CoefficientFunction((
-            airy(-x-c)*cos(y),
-            -airyp(-x-c)*cos(y),
-            -airy(-x-c)*sin(y)
+                (x+1)**ca * exp(-sqrt(ca*(ca-1))*y),
+                ca*(x+1)**(ca-1) * exp(-sqrt(ca*(ca-1))*y),
+                -sqrt(ca*(ca-1)) * (x+1)**ca * exp(-sqrt(ca*(ca-1))*y)
             ))
-        wavespeed=CoefficientFunction(1/sqrt(c+x))
+        wavespeed=CoefficientFunction((x+1))
+
     else:
+        ca=2.5
         bdd = CoefficientFunction((
-            airy(-x-y-c)*cos(sqrt(2)*z),
-            -airyp(-x-y-c)*cos(sqrt(2)*z),
-            -airyp(-x-y-c)*cos(sqrt(2)*z),
-            -airy(-x-y-c)*sin(sqrt(2)*z)*sqrt(2)
+                (x+y+1)**ca * exp(-sqrt(2*ca*(ca-1))*z),
+                ca*(x+y+1)**(ca-1) * exp(-sqrt(2*ca*(ca-1))*z),
+                ca*(x+y+1)**(ca-1) * exp(-sqrt(2*ca*(ca-1))*z),
+                -sqrt(2*ca*(ca-1))*(x+y+1)**ca * exp(-sqrt(2*ca*(ca-1))*z)
             ))
-        wavespeed=CoefficientFunction(1/sqrt(c+x+y))
+        wavespeed=CoefficientFunction((x+y+1))
 
 
     local_ctau = True
     global_ctau = 2/3
     ts = TentSlab(initmesh, method="edge", heapsize=10*1000*1000)
-    ts.SetMaxWavespeed(c)
+    ts.SetMaxWavespeed(wavespeed)
     ts.PitchTents(dt=t_step, local_ct=local_ctau, global_ct=global_ctau)
     TT=TWave(order,ts,wavespeed,qtrefftz)
     TT.SetInitial(bdd)
@@ -334,7 +199,7 @@ def SolveWaveTentsFO(initmesh, order, c, t_step):
     """
     Solve using tent pitching first order system
     >>> order = 4
-    >>> SetNumThreads(2)
+    >>> SetNumThreads(4)
     >>> c = 1
     >>> t_step = 2/sqrt(3)
     >>> initmesh = Mesh(unit_square.GenerateMesh(maxh = 0.4))
@@ -425,7 +290,7 @@ if __name__ == "__main__":
     # for i in range(3):
         # SetNumThreads(2**i)
         # start = time.time()
-        # print(TestAiryTent(order,initmesh,1)) # doctest:+ELLIPSIS
+        # print(TestQTrefftz(order,initmesh,1)) # doctest:+ELLIPSIS
         # print("time to solve on ",2**i," threads ",time.time()-start)
         # input()
 
