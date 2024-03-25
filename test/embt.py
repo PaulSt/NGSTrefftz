1298045288ee432e575f235d554c6d40f55d631e
Paul <stocker.paul@hotmail.com>
2024-03-25 08:34:52 +0100

    clean up tests

--- test/embt.py
+++ test/embt.py
@@ -2,78 +2,12 @@
 # sys.path.append(os.path.join(os.path.dirname(sys.path[0])))
 from ngsolve import *
 from ngstrefftz import *
+from dg import *
 import time
 from netgen.geom2d import unit_square
 from netgen.csg import unit_cube
-
-order = 5
-exactlap = exp(x)*sin(y)
-exactpoi = sin(x)*sin(y)
-eps = 10**-8
-mesh2d = Mesh(unit_square.GenerateMesh(maxh=0.3))
-mesh3d = Mesh(unit_cube.GenerateMesh(maxh = 1))
 SetNumThreads(3)
 
-Lap = lambda u : sum(Trace(u.Operator('hesse')))
-
-########################################################################
-# L2
-########################################################################
-def dglap(fes,bndc,rhs=0,uf=0):
-    """
-    >>> fes = L2(mesh2d, order=order,  dgjumps=True)#,all_dofs_together=True)
-    >>> a,f = dglap(fes,exactlap)
-    >>> gfu = GridFunction(fes)
-    >>> gfu.vec.data = a.mat.Inverse() * f.vec
-    >>> sqrt(Integrate((gfu-exactlap)**2, mesh2d)) # doctest:+ELLIPSIS
-    3...e-09
-    """
-    mesh = fes.mesh
-    order = fes.globalorder
-    alpha = 4
-    n = specialcf.normal(mesh.dim)
-    h = specialcf.mesh_size
-    u = fes.TrialFunction()
-    v = fes.TestFunction()
-
-    jump_u = u-u.Other()
-    jump_v = v-v.Other()
-    mean_dudn = 0.5*n * (grad(u)+grad(u.Other()))
-    mean_dvdn = 0.5*n * (grad(v)+grad(v.Other()))
-
-    a = BilinearForm(fes)
-    a += grad(u)*grad(v) * dx \
-        +alpha*order**2/h*jump_u*jump_v * dx(skeleton=True) \
-        +(-mean_dudn*jump_v-mean_dvdn*jump_u) * dx(skeleton=True) \
-        +alpha*order**2/h*u*v * ds(skeleton=True) \
-        +(-n*grad(u)*v-n*grad(v)*u)* ds(skeleton=True)
-    a.Assemble()
-
-    f = LinearForm(fes)
-    f += alpha*order**2/h*bndc*v * ds(skeleton=True) \
-         +(-n*grad(v)*bndc)* ds(skeleton=True) \
-         +rhs*v*dx
-    f.Assemble()
-
-    if uf:
-        fes2 = L2(fes.mesh, order=fes.globalorder, dgjumps=True)
-        u = fes2.TrialFunction()
-
-        jump_u = (u-u.Other())*n
-        jump_v = (v-v.Other())*n
-        mean_dudn = 0.5 * (grad(u)+grad(u.Other()))
-        mean_dvdn = 0.5 * (grad(v)+grad(v.Other()))
-
-        a2 = BilinearForm(fes2,fes,nonassemble=True)
-        a2 += grad(u)*grad(v) * dx \
-            +alpha*order**2/h*jump_u*jump_v * dx(skeleton=True) \
-            +(-mean_dudn*jump_v-mean_dvdn*jump_u) * dx(skeleton=True) \
-            +alpha*order**2/h*u*v * ds(skeleton=True) \
-            +(-n*grad(u)*v-n*grad(v)*u)* ds(skeleton=True)
-        auf = f.vec.CreateVector()
-        a2.Apply(uf.vec,auf)
-        f.vec.data = f.vec - auf
-    return a,f
 
 ########################################################################
 # PySVDTrefftz
@@ -108,13 +42,13 @@ def LocalP(A,eps=10**-9,printP=0):
 
 def PySVDTrefftz(op,fes,eps):
     """
-    # >>> fes = L2(mesh2d, order=order,  dgjumps=True)#,all_dofs_together=True)
+    # >>> fes = L2(mesh2d, order=5,  dgjumps=True)#,all_dofs_together=True)
     # >>> u,v = fes.TnT()
     # >>> uh = u.Operator("hesse")
     # >>> vh = v.Operator("hesse")
     # >>> op = (uh[0,0]+uh[1,1])*(vh[0,0]+vh[1,1])*dx
     # >>> P = PySVDTrefftz(op,fes,eps)
-    # >>> a,f = dglap(fes,exactlap)
+    # >>> a,f = dgell(fes,exactlap)
     # >>> rows,cols,vals = a.mat.COO()
     # >>> A = sp.sparse.csr_matrix((vals,(rows,cols)))
     # >>> A = A.todense()
@@ -153,7 +87,7 @@ def PySVDTrefftz(op,fes,eps):
 ########################################################################
 def testembtrefftz(fes):
     """
-    >>> fes = L2(mesh2d, order=order,  dgjumps=True)#,all_dofs_together=True)
+    >>> fes = L2(mesh2d, order=5,  dgjumps=True)#,all_dofs_together=True)
     >>> testembtrefftz(fes) # doctest:+ELLIPSIS
     8...e-09
     """
@@ -169,7 +103,7 @@ def testembtrefftz(fes):
         PP = TrefftzEmbedding(op,fes,eps)
     # spspy(PP)
     PPT = PP.CreateTranspose()
-    a,f = dglap(fes,exactlap)
+    a,f = dgell(fes,exactlap)
     TA = PPT@a.mat@PP
     TU = TA.Inverse()*(PPT*f.vec)
     tpgfu = GridFunction(fes)
@@ -183,7 +117,7 @@ def testembtrefftz(fes):
 
 def testembtrefftz_mixed(fes):
     """
-    >>> fes = L2(mesh2d, order=order,  dgjumps=True)#,all_dofs_together=True)
+    >>> fes = L2(mesh2d, order=5,  dgjumps=True)#,all_dofs_together=True)
     >>> testembtrefftz_mixed(fes) # doctest:+ELLIPSIS
     8...e-09
     """
@@ -196,7 +130,7 @@ def testembtrefftz_mixed(fes):
     with TaskManager():
         PP = TrefftzEmbedding(op,fes,test_fes=test_fes)
     PPT = PP.CreateTranspose()
-    a,f = dglap(fes,exactlap)
+    a,f = dgell(fes,exactlap)
     TA = PPT@a.mat@PP
     TU = TA.Inverse()*(PPT*f.vec)
     tpgfu = GridFunction(fes)
@@ -206,7 +140,7 @@ def testembtrefftz_mixed(fes):
 
 def testembtrefftznonsym(fes):
     """
-    >>> fes = L2(mesh2d, order=order,  dgjumps=True)#,all_dofs_together=True)
+    >>> fes = L2(mesh2d, order=5,  dgjumps=True)#,all_dofs_together=True)
     >>> testembtrefftznonsym(fes) # doctest:+ELLIPSIS
     8...e-09
     """
@@ -219,7 +153,7 @@ def testembtrefftznonsym(fes):
     with TaskManager():
         PP = TrefftzEmbedding(op,fes,eps)
     PPT = PP.CreateTranspose()
-    a,f = dglap(fes,exactlap)
+    a,f = dgell(fes,exactlap)
     TA = PPT@a.mat@PP
     TU = TA.Inverse()*(PPT*f.vec)
     tpgfu = GridFunction(fes)
@@ -228,7 +162,7 @@ def testembtrefftznonsym(fes):
 
 def testembtrefftzpoi(fes):
     """
-    >>> fes = L2(mesh2d, order=order,  dgjumps=True)#,all_dofs_together=True)
+    >>> fes = L2(mesh2d, order=5,  dgjumps=True)#,all_dofs_together=True)
     >>> testembtrefftzpoi(fes) # doctest:+ELLIPSIS
     3...e-09
     """
@@ -243,7 +177,7 @@ def testembtrefftzpoi(fes):
     with TaskManager():
         PP,ufv = TrefftzEmbedding(op,fes,lop,eps)
     PPT = PP.CreateTranspose()
-    a,f = dglap(fes,exactpoi,rhs)
+    a,f = dgell(fes,exactpoi,rhs)
     TA = PPT@a.mat@PP
     TU = TA.Inverse()*(PPT*(f.vec-a.mat*ufv))
     tpgfu = GridFunction(fes)
@@ -253,7 +187,7 @@ def testembtrefftzpoi(fes):
 
 def testembtrefftzpoi_mixed(fes):
     """
-    >>> fes = L2(mesh2d, order=order,  dgjumps=True)#,all_dofs_together=True)
+    >>> fes = L2(mesh2d, order=5,  dgjumps=True)#,all_dofs_together=True)
     >>> testembtrefftzpoi_mixed(fes) # doctest:+ELLIPSIS
     3...e-09
     """
@@ -267,7 +201,7 @@ def testembtrefftzpoi_mixed(fes):
     with TaskManager():
         PP,ufv = TrefftzEmbedding(op,fes,lop,test_fes=test_fes)
     PPT = PP.CreateTranspose()
-    a,f = dglap(fes,exactpoi,rhs)
+    a,f = dgell(fes,exactpoi,rhs)
     TA = PPT@a.mat@PP
     TU = TA.Inverse()*(PPT*(f.vec-a.mat*ufv))
     tpgfu = GridFunction(fes)
@@ -279,54 +213,9 @@ def testembtrefftzpoi_mixed(fes):
 # Helmholtz
 ########################################################################
 
-def dghelm(fes,fes2,bndc,omega):
-    mesh = fes.mesh
-    order = fes.globalorder
-    n = specialcf.normal(mesh.dim)
-    h = specialcf.mesh_size
-    alpha = 1/(omega*h)
-    beta = omega*h
-    delta = omega*h
-
-    u = fes.TrialFunction()
-    v = fes.TestFunction()
-    if fes2 is not None:
-        v = fes2.TestFunction()
-    jump_u = (u-u.Other())*n
-    jump_v = (v-v.Other())*n
-    jump_du = (grad(u)-grad(u.Other()))*n
-    jump_dv = (grad(v)-grad(v.Other()))*n
-    mean_u = 0.5 * ((u)+(u.Other()))
-    mean_du = 0.5 * (grad(u)+grad(u.Other()))
-    mean_dv = 0.5 * (grad(v)+grad(v.Other()))
-
-    a = BilinearForm(fes)
-    if fes2 is not None:
-        a = BilinearForm(fes,fes2)
-    a += mean_u*(jump_dv) * dx(skeleton=True)
-    a += 1/omega*1j*beta*jump_du*(jump_dv) * dx(skeleton=True)
-    a += -mean_du*(jump_v) * dx(skeleton=True)
-    a += omega*1j*alpha*jump_u*(jump_v) * dx(skeleton=True)
-
-    a += (1-delta)*u*(grad(v))*n * ds(skeleton=True)
-    a += 1/omega*1j*delta*(grad(u)*n)*((grad(v))*n) * ds(skeleton=True)
-    a += -delta*grad(u)*n*(v) * ds(skeleton=True)
-    a += omega*1j*(1-delta)*u*(v) * ds(skeleton=True)
-
-    f = LinearForm(fes)
-    if fes2 is not None:
-        f = LinearForm(fes2)
-    f += 1/omega*1j*delta*bndc*(grad(v))*n*ds(skeleton=True)
-    f += (1-delta)*bndc*(v)*ds(skeleton=True)
-
-    with TaskManager():
-        a.Assemble()
-        f.Assemble()
-    return a,f
-
 def testembtrefftzhelm(fes):
     """
-    >>> fes = L2(mesh2d, order=order,  dgjumps=True, complex=True)
+    >>> fes = L2(mesh2d, order=5,  dgjumps=True, complex=True)
     >>> testembtrefftzhelm(fes) # doctest:+ELLIPSIS
     8...e-10
     """
@@ -361,7 +250,7 @@ def testembtrefftzhelm(fes):
 
 def testembtrefftzfes(mesh,order):
     """
-    >>> testembtrefftzfes(mesh2d,order) # doctest:+ELLIPSIS
+    >>> testembtrefftzfes(mesh2d,5) # doctest:+ELLIPSIS
     3...e-09
     """
     # exact = sin(10*x)*sin(10*y)
@@ -380,7 +269,7 @@ def testembtrefftzfes(mesh,order):
     uf.vec.data = etfes.SetOp(op,lf=lop,eps=eps)
 
     # etfes = Compress(etfes, etfes.FreeDofs())
-    a,f = dglap(etfes,exactpoi,rhs,uf)
+    a,f = dgell(etfes,exactpoi,rhs,uf)
     # import pdb; pdb.set_trace()
 
     inv = a.mat.Inverse(inverse="sparsecholesky")
