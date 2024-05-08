06e6e3af4c3923bf043d1c08e57329c794daea28
Paul Stocker <stocker.paul@hotmail.com>
2024-05-08 22:03:29 +0200

    new cmake (#86)

--- docs/conf.py
+++ docs/conf.py
@@ -2,54 +2,9 @@ from os.path import dirname, isdir, join
 import os
 import re
 import subprocess
-def get_version():
-    """
-    Gets the current version number.
-    If in a git repository, it is the current git tag.
-    Otherwise it is the one contained in the PKG-INFO file.
-    """
-    version_re = re.compile('^Version: (.+)$', re.M)
-    d = dirname(__file__)+'/..'
 
-    if isdir(join(d, '.git')):
-        # Get the version using "git describe".
-        cmd = 'git describe --always --tags --match v[0-9]*'.split()
-        try:
-            version = subprocess.check_output(cmd).decode().strip()
-        except subprocess.CalledProcessError:
-            print('Unable to get version number from git tags')
-            exit(1)
-
-        # PEP 386 compatibility
-        if '-' in version:
-            version = '.post'.join(version.split('-')[:2])
-
-        # Don't declare a version "dirty" merely because a time stamp has
-        # changed. If it is dirty, append a ".dev1" suffix to indicate a
-        # development revision after the release.
-        with open(os.devnull, 'w') as fd_devnull:
-            subprocess.call(['git', 'status'],
-                            stdout=fd_devnull, stderr=fd_devnull)
-
-        cmd = 'git diff-index --name-only HEAD'.split()
-        try:
-            dirty = subprocess.check_output(cmd).decode().strip()
-        except subprocess.CalledProcessError:
-            print('Unable to get git index status')
-            exit(1)
-
-        # if dirty != '':
-            # version += '.dev1'
-
-        # strip the v for pypi
-        version = version[1:]
-
-    else:
-        # Extract the version from the PKG-INFO file.
-        with open(join(d, 'PKG-INFO')) as f:
-            version = version_re.search(f.read()).group(1)
-
-    return version
+from setuptools_scm import get_version
+_cmake_args = []
 
 
 # Configuration file for the Sphinx documentation builder.
@@ -76,7 +31,7 @@ copyright = '2022, Paul Stocker'
 author = 'Paul Stocker'
 
 # The full version, including alpha/beta/rc tags
-release = get_version()
+release = get_version(root='..', relative_to=__file__).split('+')[0]
 
 # -- General configuration ---------------------------------------------------
 
