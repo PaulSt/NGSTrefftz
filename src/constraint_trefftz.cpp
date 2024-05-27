1d577a6dec68bda0f63ff11a607bf69d8ec5b606
Johann Carl Meyer <info@johannc.de>
2024-05-27 14:34:36 +0200

    move functions to trefftz_helper

--- src/constraint_trefftz.cpp
+++ src/constraint_trefftz.cpp
@@ -19,63 +19,9 @@
 #include <fespace.hpp>
 #include <vector>
 
+#include "trefftz_helper.hpp"
+
 using namespace ngfem;
-
-/// @param bf symblic representation of a bilinear form
-/// @param bfis stores the calculated BilinearFormIntegrators
-void calculateBilinearFormIntegrators (
-    const SumOfIntegrals &bf,
-    Array<shared_ptr<BilinearFormIntegrator>> bfis[4])
-{
-  for (auto icf : bf.icfs)
-    {
-      DifferentialSymbol &dx = icf->dx;
-      bfis[dx.vb] += icf->MakeBilinearFormIntegrator ();
-    }
-}
-
-/// @param lf symblic representation of a linear form
-/// @param lfis stores the calculated LinearFormIntegrators
-void calculateLinearFormIntegrators (
-    SumOfIntegrals &lf, Array<shared_ptr<LinearFormIntegrator>> lfis[4])
-{
-  for (auto icf : lf.icfs)
-    {
-      DifferentialSymbol &dx = icf->dx;
-      lfis[dx.vb] += icf->MakeLinearFormIntegrator ();
-    }
-}
-
-/// decides, if the given finite element space
-/// has hidden degrees of freedom.
-///
-/// @param fes finite element space
-bool fesHasHiddenDofs (const ngcomp::FESpace &fes)
-{
-  const size_t ndof = fes.GetNDof ();
-  bool has_hidden_dofs = false;
-  for (ngcomp::DofId d = 0; d < ndof; d++)
-    if (ngcomp::HIDDEN_DOF == fes.GetDofCouplingType (d))
-      return true;
-  return false;
-}
-
-/// Tests, if the given bilinear form is defined on the given element.
-///
-/// @param bf bilinear form
-/// @param mesh_element local mesh element
-bool bfIsDefinedOnElement (const SumOfIntegrals &bf,
-                           const ngcomp::Ngs_Element &mesh_element)
-{
-  for (auto icf : bf.icfs)
-    {
-      if (icf->dx.vb == VOL)
-        if ((!icf->dx.definedonelements)
-            || (icf->dx.definedonelements->Test (mesh_element.Nr ())))
-          return true;
-    }
-  return false;
-}
 
 namespace ngcomp
 {
@@ -213,6 +159,9 @@ namespace ngcomp
           // auto el_b2 = copbf_rhs.GetInnerMatrix ();
 
           auto el_a = FlatMatrix<SCAL> (); // #TODO: define height and width
+          FlatMatrix<SCAL> elmat_op (dofs.Size (), dofs.Size (), local_heap);
+          calculateElementMatrix (elmat_op, op_integrators[VOL], *mesh_access,
+                                  el_id, *fes, *fes, local_heap);
         });
 
     return element_matrices;
