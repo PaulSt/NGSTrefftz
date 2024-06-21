c35fcf7ae13ba5f2a84c2ea794421cf91027c357
Paul Stocker <stocker.paul@hotmail.com>
2024-06-21 12:56:01 +0200

    update cmake, fix mkl (#98)

--- src/python_trefftz.cpp
+++ src/python_trefftz.cpp
@@ -3,8 +3,8 @@
 #include <fem.hpp>
 #include <comp.hpp>
 // using namespace ngsolve;
-// #include <tents.hpp>
-#include "constraint_trefftz.hpp"
+#include <tents.hpp>
+#include <python_tents.cpp>
 #include "trefftzfespace.hpp"
 #include "specialcoefficientfunction.hpp"
 #include "specialintegrator.hpp"
@@ -21,6 +21,7 @@ PYBIND11_MODULE (_trefftz, m)
   m.attr ("__name__") = "ngstrefftz";
   m.attr ("__package__") = "ngstrefftz";
 
+  ExportTents (m);
   ExportTrefftzFESpace (m);
   ExportSpecialCoefficientFunction (m);
   ExportSpecialIntegrator (m);
